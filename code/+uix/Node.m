5c5f1d33a87c4978c3c5d0e34c296911321837e8
David Sampson <dsampson@mathworks.com>
2013-09-03 16:01:50 +0000

    Finished uix.Node
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@400 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/Node.m
+++ code/+uix/Node.m
@@ -1,122 +1,98 @@
 classdef Node < handle
     
     properties( SetAccess = protected )
-        Peer
+        Object
         Children = uix.Node.empty( [0 1] )
     end
     
     properties( Access = protected )
-        Listeners
+        Listeners = event.listener.empty( [0 1] )
     end
     
     events( NotifyAccess = protected )
         ChildAdded
         ChildRemoved
+        Deleted
     end
     
     methods
         
-        function obj = Node( peer )
+        function obj = Node( object )
             
-            % Check
-            assert( isvalid( peer ), 'Invalid handle.' )
+            % Create peer
+            eventSource = uix.EventSource( object );
+            
+            % Store properties
+            obj.Object = object;
             
             % Add existing children
-            children = hgGetTrueChildren( peer );
+            children = hgGetTrueChildren( object );
             for ii = 1:numel( children )
                 child = children(ii);
-                fprintf( 1, '+ Added existing child %s to %s\n', class( child ), class( peer ) );
+                uix.Node.message( '  Created %s for existing child node %s\n', ...
+                    class( obj ), class( child ) )
                 obj.Children(end+1,:) = uix.Node( child );
             end
             
-            % Create child listeners
-            listeners = { ...
-                event.listener( peer, 'ObjectChildAdded', @obj.onChildAdded ); ...
-                event.listener( peer, 'ObjectChildRemoved', @obj.onChildRemoved ); ...
-                };
-            
-            % Store properties
-            obj.Peer = peer;
-            obj.Listeners = listeners;
+            % Add listeners
+            obj.Listeners(end+1,:) = event.listener( eventSource, ...
+                'ObjectChildAdded', @obj.onChildAdded );
+            obj.Listeners(end+1,:) = event.listener( eventSource, ...
+                'ObjectChildRemoved', @obj.onChildRemoved );
+            obj.Listeners(end+1,:) = event.listener( eventSource, ...
+                'ObjectDeleted', @obj.onDeleted );
             
         end % constructor
         
         function delete( obj )
             
-            fprintf( 1, '! Deleted node %s\n', class( obj.Peer ) );
+            uix.Node.message( 'Deleted %s for %s\n', class( obj ), class( obj.Object ) )
             
         end % destructor
         
     end % structors
     
-    methods( Access = protected )
+    methods
         
         function onChildAdded( obj, source, eventData )
             
             child = eventData.Child;
-            fprintf( 1, '+ Added %s to %s\n', class( child ), class( source ) );
-            obj.Children(end+1,:) = uix.Node( child );
-            notify( obj, 'ChildAdded', uix.ChildEvent( child ) )
+            uix.Node.message( 'Added %s to %s\n', class( child ), class( source.Object ) )
+            node = uix.Node( child );
+            obj.Children(end+1,:) = node;
+            notify( obj, 'ChildAdded', uix.ChildEvent( node ) )
             
         end % onChildAdded
         
         function onChildRemoved( obj, source, eventData )
             
             child = eventData.Child;
-            fprintf( 1, '- Removed %s from %s\n', class( child ), class( source ) );
-            index = find( [obj.Children.Peer] == child );
-            switch numel( index )
-                case 0
-                    error( 'uix:InvalidState', 'Node not found' )
-                case 1
-                    obj.Children(index,:) = [];
-                otherwise
-                    error( 'uix:InvalidState', 'Duplicate nodes' )
-            end
-            notify( obj, 'ChildRemoved', uix.ChildEvent( child ) )
+            uix.Node.message( 'Removed %s from %s\n', class( child ), class( source.Object ) )
+            tf = vertcat( obj.Children.Object ) == child;
+            node = obj.Children(tf,:);
+            assert( numel( node ) == 1 )
+            obj.Children(tf,:) = [];
+            notify( obj, 'ChildRemoved', uix.ChildEvent( node ) )
             
         end % onChildRemoved
         
-        function onDeleted( obj, ~, ~ )
+        function onDeleted( obj, source, ~ )
             
-            obj.delete()
+            uix.Node.message( 'Deleted %s\n', class( source.Object ) )
+            notify( obj, 'Deleted' )
             
         end % onDeleted
         
     end % event handlers
     
-    methods
+    methods( Static )
         
-        function show( obj )
+        function message( varargin )
             
-            nShow( obj, 0 )
-            function nShow( obj, level )
-                fprintf( 1, '%s', repmat( ' ', [1 2*level] ) );
-                fprintf( 1, '%s', class( obj.Peer ) );
-                fprintf( 1, '\n' );
-                for ii = 1:numel( obj.Children )
-                    nShow( obj.Children(ii), level + 1 )
-                end
-            end % nShow
+            % fprintf( 1, varargin{:} );
             
-        end % show
+        end % message
         
-    end
+    end % static methods
     
 end % classdef
-
-function p = hgGetTrueParent( c )
-
-p = nGetParent( c, ancestor( c, 'Root' ) );
-
-    function p = nGetParent( o, r )
-        ch = hgGetTrueChildren( r );
-        for ii = 1:numel( ch )
-            if 
-        
-        
-        
-        
-    end
-
-end % iGetParent
