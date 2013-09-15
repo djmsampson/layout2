030bd20eb64746168e8bc572075e5041c07a7c06
David Sampson <dsampson@mathworks.com>
2013-09-15 12:51:40 +0000

    Updated uix.Container to use uix.ChildObserver
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@421 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/Container.m
+++ code/+uix/Container.m
@@ -6,13 +6,7 @@ classdef Container < matlab.ui.container.internal.UIContainer
     
     properties( Access = protected )
         Contents_ = matlab.graphics.GraphicsPlaceholder.empty( [0 1] )
-        Listeners = cell( [0 1] )
-        ChildListeners = cell( [0 1] )
-    end
-    
-    events( NotifyAccess = private )
-        ChildAdded
-        ChildRemoved
+        Listeners = event.listener.empty( [0 1] )
     end
     
     methods
@@ -22,16 +16,15 @@ classdef Container < matlab.ui.container.internal.UIContainer
             % Call superclass constructor
             obj@matlab.ui.container.internal.UIContainer( varargin{:} );
             
-            % Set up listeners
-            obj.Listeners{end+1,:} = event.listener( ...
-                obj, 'ObjectChildAdded', @obj.onObjectChildAdded );
-            obj.Listeners{end+1,:} = event.listener( ...
-                obj, 'ObjectChildRemoved', @obj.onObjectChildRemoved );
-            obj.Listeners{end+1,:} = event.listener( ...
-                obj, 'ChildAdded', @obj.onChildAdded );
-            obj.Listeners{end+1,:} = event.listener( ...
-                obj, 'ChildRemoved', @obj.onChildRemoved );
-            obj.Listeners{end+1,:} = event.listener( ...
+            % Create child listeners
+            childObserver = uix.ChildObserver( obj );
+            obj.Listeners(end+1,:) = event.listener( ...
+                childObserver, 'ChildAdded', @obj.onChildAdded );
+            obj.Listeners(end+1,:) = event.listener( ...
+                childObserver, 'ChildRemoved', @obj.onChildRemoved );
+            
+            % Create resize listener
+            obj.Listeners(end+1,:) = event.listener( ...
                 obj, 'SizeChange', @obj.onSizeChanged );
             
         end % constructor
@@ -73,42 +66,6 @@ classdef Container < matlab.ui.container.internal.UIContainer
         
     end % protected methods
     
-    methods( Access = private )
-        
-        function onObjectChildAdded( obj, ~, eventData )
-            
-            child = eventData.Child;
-            if ishghandle( child )
-                % Raise event
-                notify( obj, 'ChildAdded', uix.ChildEvent( child ) )
-            elseif isa( child, 'matlab.graphics.primitive.canvas.Canvas' )
-                % Add listeners
-                obj.ChildListeners{end+1,:} = event.listener( ...
-                    child, 'ObjectChildAdded', @obj.onObjectChildAdded );
-                obj.ChildListeners{end+1,:} = event.listener( ...
-                    child, 'ObjectChildRemoved', @obj.onObjectChildRemoved );
-            else
-                % Warn
-                warning( 'uix:InvalidOperation', ...
-                    'Unsupported addition of %s.', class( child ) )
-            end
-            
-        end % onObjectChildAdded
-        
-        function onObjectChildRemoved( obj, ~, eventData )
-            
-            if strcmp( obj.BeingDeleted, 'on' ), return, end
-            
-            child = eventData.Child;
-            if ishghandle( child )
-                % Raise event
-                notify( obj, 'ChildRemoved', uix.ChildEvent( child ) )
-            end
-            
-        end % onObjectChildRemoved
-        
-    end % private event handlers
-    
     methods( Access = protected )
         
         function onChildAdded( obj, ~, eventData )
@@ -127,7 +84,7 @@ classdef Container < matlab.ui.container.internal.UIContainer
             if strcmp( obj.BeingDeleted, 'on' ), return, end
             
             % Remove from contents
-            obj.Contents_(find( obj.Contents_ == eventData.Child ),:) = []; %#ok<FNDSB>
+            obj.Contents_(obj.Contents_==eventData.Child,:) = [];
             
             % Call redraw
             obj.redraw()
@@ -143,4 +100,4 @@ classdef Container < matlab.ui.container.internal.UIContainer
         
     end % event handlers
     
-end % end
+end % classdef
