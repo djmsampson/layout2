f45873d49ec09e7be0e47d149f4eef64939c0949
David Sampson <dsampson@mathworks.com>
2013-09-15 13:19:53 +0000

    Minor change to pvchk
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@422 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/pvchk.m
+++ code/+uix/pvchk.m
@@ -2,7 +2,7 @@ function pvchk( pv )
 
 if rem( numel( pv ), 2 ) ~= 0
     MException( 'uix:InvalidArgument' ).throwAsCaller()
-elseif ~all( cellfun( @isvarname, pv(1:2:end) ) )
+elseif ~all( cellfun( @ischar, pv(1:2:end) ) )
     MException( 'uix:InvalidArgument' ).throwAsCaller()
 elseif numel( pv(1:2:end) ) ~= numel( unique( pv(1:2:end) ) )
     MException( 'uix:InvalidArgument' ).throwAsCaller()
