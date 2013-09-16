64a6f718916bb9154c29134e540dcc6f44ae4255
David Sampson <dsampson@mathworks.com>
2013-09-16 09:12:07 +0000

    Early work on HBox
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@425 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/HBox.m
+++ code/+uix/HBox.m
@@ -2,6 +2,7 @@ classdef HBox < uix.Container
     
     properties
         Padding = 0
+        Spacing = 0
     end
     
     properties( Access = public, Dependent )
@@ -34,7 +35,7 @@ classdef HBox < uix.Container
     methods
         
         function set.Padding( obj, value )
-           
+            
             % Check
             assert( isa( value, 'double' ) && isscalar( value ) && ...
                 isreal( value ) && ~isinf( value ) && ...
@@ -49,6 +50,23 @@ classdef HBox < uix.Container
             obj.redraw()
             
         end % set.Padding
+        
+        function set.Spacing( obj, value )
+            
+            % Check
+            assert( isa( value, 'double' ) && isscalar( value ) && ...
+                isreal( value ) && ~isinf( value ) && ...
+                ~isnan( value ) && value >= 0, ...
+                'uix:InvalidPropertyValue', ...
+                'Property ''Spacing'' must be a non-negative scalar.' )
+            
+            % Set
+            obj.Spacing = value;
+            
+            % Redraw
+            obj.redraw()
+            
+        end % set.Spacing
         
         function value = get.Sizes( obj )
             
@@ -82,6 +100,42 @@ classdef HBox < uix.Container
     
     methods( Access = protected )
         
+        function redraw( obj )
+            
+            % Abort for parentless containers
+            if isempty( obj.Parent ), return, end
+            
+            % Compute container bounds
+            pBounds = hgconvertunits( ancestor( obj, 'figure' ), ...
+                obj.Position, obj.Units, 'pixels', obj.Parent );
+            
+            % Compute sizes
+            mSizes = obj.Sizes;
+            pPadding = obj.Padding;
+            pSpacing = obj.Spacing;
+            pSizes = repmat( 100, size( mSizes ) ); % TODO
+            
+            % Compute positions
+            pPositions = zeros( [size( mSizes, 1 ) 4] );
+            pPositions(:,1) = cumsum( [0; pSizes(1:end-1,:)] ) + pPadding + ...
+                cumsum( repmat( pSpacing, size( mSizes ) ) ) - pSpacing;
+            pPositions(:,2) = pPadding;
+            pPositions(:,3) = pSizes;
+            pPositions(:,4) = max( pBounds(4) - 2 * pPadding, 1 );
+            
+            % Set positions
+            for ii = 1:size( mSizes, 1 )
+                child = obj.Contents(ii);
+                child.Units = 'pixels';
+                child.Position = pPositions(ii,:);
+            end
+            
+        end % redraw
+        
+    end % protected methods
+    
+    methods( Access = protected )
+        
         function onChildAdded( obj, source, eventData )
             
             % Add to sizes
@@ -98,7 +152,8 @@ classdef HBox < uix.Container
             if strcmp( obj.BeingDeleted, 'on' ), return, end
             
             % Remove from sizes
-            obj.Sizes_(find( obj.Contents_ == eventData.Child ),:) = []; %#ok<FNDSB>
+            tf = obj.Contents_ == eventData.Child;
+            obj.Sizes_(tf,:) = [];
             
             % Call superclass method
             onChildRemoved@uix.Container( obj, source, eventData )
