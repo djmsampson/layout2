d5a80ea0559e28a3adeb8384fb6598f46c375eb9
David Sampson <dsampson@mathworks.com>
2013-09-16 21:23:34 +0000

    Added getPixelPositions
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@429 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/+uix/getPixelPositions.m
@@ -0,0 +1,34 @@
+function [pPositions, pSizes] = getPixelPositions( pTotal, mSizes, pMinimumSizes, pPadding, pSpacing )
+
+n = numel( mSizes ); % number of children
+
+if n == 0
+    
+    pSizes = zeros( [n 2] );
+    pPositions = zeros( [n 2] );
+    
+else
+    
+    % Initialize
+    pSizes = NaN( [n 1] );
+    
+    % Allocate absolute sizes
+    a = mSizes >= 0; % absolute
+    s = mSizes < pMinimumSizes; % small
+    pSizes(a&~s) = mSizes(a&~s);
+    pSizes(a&s) = pMinimumSizes(a&s);
+    
+    % Allocate relative sizes
+    pTotalRelative = max( pTotal - 2 * pPadding - (n-1) * pSpacing - ...
+        sum( pSizes(a) ), 0 );
+    s = pTotalRelative * mSizes / sum( mSizes(~a) ) < pMinimumSizes; % small
+    pSizes(~a&s) = pMinimumSizes(~a&s);
+    pTotalRelative = max( pTotal - 2 * pPadding - (n-1) * pSpacing - ...
+        sum( pSizes(a|s) ), 0 );
+    pSizes(~a&~s) = pTotalRelative * mSizes(~a&~s) / sum( mSizes(~a&~s) );
+    
+    % Compute positions
+    pPositions = [cumsum( [0; pSizes(1:end-1,:)] ) + pPadding + ...
+        pSpacing * transpose( 0:n-1 ), pSizes];
+    
+end % getPixelPositions
