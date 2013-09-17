4ba7148b1b204c51f4aa7ce0268ce89aa90d1ddd
David Sampson <dsampson@mathworks.com>
2013-09-17 11:32:48 +0000

    Added doc for calcPixelPositions
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@435 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/calcPixelPositions.m
+++ code/+uix/calcPixelPositions.m
@@ -1,10 +1,28 @@
-function [pPositions, pSizes] = calcPixelPositions( pTotal, mSizes, pMinimumSizes, pPadding, pSpacing )
+function pPositions = calcPixelPositions( pTotal, mSizes, pMinimumSizes, pPadding, pSpacing )
+%calcPixelPositions  Calculate child positions in pixels
+%
+%  positions = uix.calcPixelPositions(total,sizes,minSizes,padding,spacing)
+%  computes child positions given total available size (in pixels), child
+%  sizes (in pixels and/or relative), minimum child sizes (in pixels),
+%  padding (in pixels) and spacing (in pixels).  positions is an n-by-2
+%  matrix, where n is the number of children, with the first column
+%  corresponding to left/bottom, and the second column corresponding to
+%  width/height.
+%
+%  Notes:
+%  * All children are at least as large as the minimum specified size
+%  * Relative sizes are respected for children larger than then minimum
+%  specified size
+%  * Children may extend beyond the total available size if the minimum
+%  sizes, padding and spacing are too large
+
+%  Copyright 2009-2013 The MathWorks, Inc.
+%  $Revision: 383 $ $Date: 2013-04-29 11:44:48 +0100 (Mon, 29 Apr 2013) $
 
 n = numel( mSizes ); % number of children
 
 if n == 0
     
-    pSizes = zeros( [n 2] );
     pPositions = zeros( [n 2] );
     
 else
