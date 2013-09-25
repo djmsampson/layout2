bad87625d18e58584d84b45083e9a64137ef532d
David Sampson <dsampson@mathworks.com>
2013-09-25 10:11:28 +0000

    Updated uix.getPixelPosition
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@452 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/getPixelPosition.m
+++ code/+uix/getPixelPosition.m
@@ -1,59 +1,31 @@
-function position = getPixelPosition(h,recursive)
-% GETPIXELPOSITION Get the position of an HG object in pixel units.
-%   GETPIXELPOSITION(HANDLE) gets the position of the object specified by
-%   HANDLE in pixel units.
-%
-%   GETPIXELPOSITION(HANDLE, RECURSIVE) gets the position as above. If
-%   RECURSIVE is true, the returned position is relative to the parent
-%   figure of HANDLE.
-%
-%   POSITION = GETPIXELPOSITION(...) returns the pixel position in POSITION.
-%
-%   Example:
-%       f = figure;
-%       p = uipanel('Position', [.2 .2 .6 .6]);
-%       h1 = uicontrol(p, 'Units', 'normalized', 'Position', [.1 .1 .5 .2]);
-%       % Get pixel position w.r.t the parent uipanel
-%       pos1 = getpixelposition(h1)
-%       % Get pixel position w.r.t the parent figure using the recursive flag
-%       pos2 = getpixelposition(h1, true)
-%
-%   See also SETPIXELPOSITION, UICONTROL, UIPANEL
+function position = getPixelPosition( h, recursive )
 
-% Copyright 1984-2010 The MathWorks, Inc.
-% $Revision: 1.1.6.11 $ $Date: 2011/03/09 07:05:43 $
-  
-% Verify that getpixelposition is given between 1 and 2 arguments
-persistent pixOrigin;
+% Check inputs
+narginchk( 1, 2 )
 
-% @todo - Pixel origin has crept into MATLAB code. This should be made a
-% built-in.
-if isempty(pixOrigin)
-    pixOrigin = 1;
+% Get position
+figure = ancestor( h, 'figure' );
+if isempty( figure ) % unrooted
+    position = [NaN NaN NaN NaN];
+else % in figure
+    % Handle inputs
+    if nargin < 2
+        recursive = false;
+    end
+    % Get position relative to parent
+    parent = h.Parent;
+    position = hgconvertunits( figure, h.Position, h.Units, 'pixels', parent );
+    % Add offsets for each parent
+    if recursive && ~isequal( h, figure )
+        while ~isequal( parent, figure )
+            % Compute offset relative to parent
+            h = parent;
+            parent = h.Parent;
+            offset = hgconvertunits( figure, h.Position, h.Units, 'pixels', parent );
+            % Adjust position by offset
+            position = position + [offset(1) offset(2) 0 0] - [1 1 0 0];
+        end
+    end
 end
 
-error(nargchk(1, 2, nargin, 'struct')) 
-
-% Verify that "h" is a handle
-if ~ishghandle(h)
-    error(message('MATLAB:getpixelposition:InvalidHandle'))
-end
-
-if nargin < 2
-  recursive = false;
-end
-
-parent = get(h,'Parent');
-
-% Use hgconvertunits to get the position in pixels (avoids recursion
-% due to unit changes triggering resize events which re-call
-% getpixelposition)
-position = hgconvertunits(ancestor(h,'figure'),get(h,'Position'),get(h,'Units'),...
-          'Pixels',parent);      
-
-if recursive && ~ishghandle(h,'figure') && ~ishghandle(parent,'figure')
- parentPos = uix.getPixelPosition(parent, recursive); 
- position = position + [parentPos(1) parentPos(2) 0 0] - [pixOrigin pixOrigin 0 0];
-end
-  
-
+end % getPixelPosition
