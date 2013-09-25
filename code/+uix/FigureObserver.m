631819a6ea78ce57722502f97bdc0e426890b134
David Sampson <dsampson@mathworks.com>
2013-09-25 08:50:26 +0000

    Updated uix.FigureObserver, removed uix.FigureEvent
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@449 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/FigureObserver.m
+++ code/+uix/FigureObserver.m
@@ -1,8 +1,11 @@
 classdef( Hidden, Sealed ) FigureObserver < handle
     
-    properties( Access = private )
-        Object
+    properties( SetAccess = private )
+        Subject
         Figure
+    end
+    
+    properties( Access = private )
         ParentListeners = event.proplistener.empty( [0 1] )
     end
     
@@ -12,22 +15,22 @@ classdef( Hidden, Sealed ) FigureObserver < handle
     
     methods
         
-        function obj = FigureObserver( object )
+        function obj = FigureObserver( subject )
             %uix.FigureObserver  Figure observer
             %
             %  fo = uix.FigureObserver(o) creates a figure observer for the
-            %  graphics object o.  A figure observer raises an event
+            %  graphics subject o.  A figure observer raises an event
             %  FigureChanged when ancestor(o,'figure') changes.  o
             
             % Check
-            assert( ishghandle( object ) && ...
-                isequal( size( object ), [1 1] ) && ...
-                ~isa( object, 'matlab.ui.Figure' ) && ...
-                ~isequal( object, groot() ), 'uix.InvalidArgument', ...
-                'Object must be a graphics object.' )
+            assert( ishghandle( subject ) && ...
+                isequal( size( subject ), [1 1] ) && ...
+                ~isa( subject, 'matlab.ui.Figure' ) && ...
+                ~isequal( subject, groot() ), 'uix.InvalidArgument', ...
+                'Subject must be a graphics subject.' )
             
             % Store properties
-            obj.Object = object;
+            obj.Subject = subject;
             
             % Force update
             obj.update()
@@ -42,7 +45,7 @@ classdef( Hidden, Sealed ) FigureObserver < handle
             %update  Update figure observer
             
             % Identify ancestors
-            parents = obj.Object; % initialize
+            parents = obj.Subject; % initialize
             while true
                 parent = parents(end,:).Parent;
                 if isempty( parent ) || isa( parent, 'matlab.ui.Figure' )
@@ -54,9 +57,12 @@ classdef( Hidden, Sealed ) FigureObserver < handle
             end
             
             % Create listeners
-            parentListeners = event.proplistener( parents, ...
-                findprop( parents(1), 'Parent' ), 'PostSet', ...
-                @obj.onParentChanged );
+            for ii = 1:numel( parents )
+                parent = parents(ii);
+                parentListeners(ii,:) = event.proplistener( parent, ...
+                    findprop( parent, 'Parent' ), 'PostSet', ...
+                    @obj.onParentChanged ); %#ok<AGROW>
+            end
             
             % Store properties
             obj.Figure = figure;
@@ -79,7 +85,7 @@ classdef( Hidden, Sealed ) FigureObserver < handle
             % Raise event
             newFigure = obj.Figure;
             if ~isequal( oldFigure, newFigure )
-                notify( obj, 'FigureChanged', uix.FigureEvent( newFigure ) )
+                notify( obj, 'FigureChanged' )
             end
             
         end % onParentChanged
