7bdd2eac21ebc99db880a38913996c475b38d765
David Sampson <dsampson@mathworks.com>
2013-09-30 14:34:23 +0000

    Working version of uix.LocationObserver, but with no support for docking/undocking yet
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@464 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/LocationObserver.m
+++ code/+uix/LocationObserver.m
@@ -5,11 +5,9 @@ classdef ( Hidden, Sealed ) LocationObserver < handle
         Location = [NaN NaN NaN NaN]
     end
     
-    properties % ( Access = private )
+    properties( Access = private )
         Figure = matlab.graphics.GraphicsPlaceholder.empty( [0 0] )
         Ancestors = matlab.graphics.GraphicsPlaceholder.empty( [0 1] )
-        Units = cell( [0 1] )
-        Positions = zeros( [0 4] )
         Offsets = zeros( [0 2] )
         Extent = [NaN NaN]
         LocationListeners = event.listener.empty( [0 1] )
@@ -23,13 +21,26 @@ classdef ( Hidden, Sealed ) LocationObserver < handle
     methods
         
         function obj = LocationObserver( varargin )
+            %uix.LocationObserver  Location observer
+            %
+            %  o = uix.LocationObserver(s) creates a location observer for
+            %  the subject s.
+            %
+            %  o = uix.LocationObserver(a,f) creates a location observer
+            %  for the top-to-bottom ancestry a in the figure f.
+            %
+            %  A location observer assumes a fixed ancestry.  Use an
+            %  ancestry observer to monitor changes to ancestry, and create
+            %  a new location observer when ancestry changes.
             
-            % Identify ancestors
+            % Handle inputs
             switch nargin
                 case 1
+                    % Identify ancestors
                     subject = varargin{1};
                     [ancestors, figure] = uix.ancestors( subject );
                 case 2
+                    % Ancestors and figure specified
                     ancestors = varargin{1};
                     subject = ancestors(end);
                     figure = varargin{2};
@@ -37,16 +48,13 @@ classdef ( Hidden, Sealed ) LocationObserver < handle
                     narginchk( 1, 2 )
             end
             
-            % Store subject and ancestors
+            % Store subject, ancestors and figure
             obj.Subject = subject;
             obj.Ancestors = ancestors;
             obj.Figure = figure;
             
             % Stop early for unrooted subjects
             if isempty( figure ), return, end
-            
-            % Store figure
-            obj.Figure = ancestors(1);
             
             % Force update
             obj.update()
@@ -70,48 +78,64 @@ classdef ( Hidden, Sealed ) LocationObserver < handle
     
     methods( Access = private )
         
-        function update( obj, source, eventData )
+        function update( obj, source )
+            %update  Update location observer
+            %
+            %  o.update() updates the state of the location observer from
+            %  scratch.
+            %
+            %  o.update(a) updates the state of the location observer in
+            %  response to an event on the ancestor a.
             
-            % Print debug information
-            if nargin == 3
-                fprintf( 1, 'Update due to %s on %s\n', eventData.EventName, class( source ) )
-            end
-            
-            % Retrieve old values
-            oldUnits = obj.Units;
-            oldPositions = obj.Positions;
-            oldOffsets = obj.Offsets;
-            oldExtent = obj.Extent;
-            oldLocation = obj.Location;
-            
-            % Compute modified values
+            % Retrieve ancestors, parents and figure
             ancestors = obj.Ancestors;
             parents = [groot(); ancestors(1:end-1,:)];
             figure = obj.Figure;
-            newUnits = get( ancestors, 'Units' );
-            cNewPositions = get( ancestors, 'Position' );
-            newPositions = vertcat( cNewPositions{:} );
-            n = numel( ancestors );
-            newOffsets = zeros( [n 2] ); % initialize
-            for ii = 1:n
-                offset = hgconvertunits( figure, newPositions(ii,:), ...
-                    newUnits{ii}, 'pixels', parents(ii) );
-                newOffsets(ii,:) = offset(1:2) - 1;
-            end
-            newExtent = offset(3:4);
-            newLocation = [sum( newOffsets, 1 ) newExtent];
             
-            % Store new values
-            obj.Units = newUnits;
-            obj.Positions = newPositions;
-            obj.Offsets = newOffsets;
-            obj.Extent = newExtent;
-            obj.Location = newLocation;
+            if nargin == 1 % recompute from scratch
+                
+                % Compute units, positions and offsets of all ancestors
+                units = get( ancestors, 'Units' );
+                cPositions = get( ancestors, 'Position' );
+                positions = vertcat( cPositions{:} );
+                n = numel( ancestors );
+                offsets = zeros( [n 2] ); % initialize
+                for ii = 1:n
+                    pixel = hgconvertunits( figure, positions(ii,:), ...
+                        units{ii}, 'pixels', parents(ii) );
+                    offsets(ii,:) = pixel(1:2) - 1;
+                end
+                extent = pixel(3:4);
+                
+            else % specified modified ancestor
+                
+                % Compute units, position and offset of modified ancestor
+                tf = ancestors == source;
+                ancestor = ancestors(tf);
+                parent = parents(tf);
+                pixel = hgconvertunits( figure, ancestor.Position, ...
+                    ancestor.Units, 'pixels', parent );
+                offset = pixel(1:2) - 1;
+                offsets = obj.Offsets;
+                offsets(tf,:) = offset;
+                if tf(end) % subject
+                    extent = pixel(3:4);
+                else
+                    extent = obj.Extent;
+                end
+                
+            end
+            
+            % Compute location
+            location = [sum( offsets, 1 ) extent];
+            
+            % Store properties
+            obj.Offsets = offsets;
+            obj.Extent = extent;
+            obj.Location = location;
             
             % Raise event
-            if ~isequal( oldLocation, newLocation )
-                notify( obj, 'LocationChange' )
-            end
+            notify( obj, 'LocationChange' )
             
         end % update
         
@@ -119,17 +143,17 @@ classdef ( Hidden, Sealed ) LocationObserver < handle
     
     methods( Access = private )
         
-        function onLocationChange( obj, source, eventData )
+        function onLocationChange( obj, source, ~ )
             
             % Update
-            obj.update( source, eventData )
+            obj.update( source )
             
         end % onLocationChange
         
-        function onSizeChange( obj, source, eventData )
+        function onSizeChange( obj, source, ~ )
             
             % Update
-            obj.update( source, eventData )
+            obj.update( source )
             
         end % onSizeChange
         
