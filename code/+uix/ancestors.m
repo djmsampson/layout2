e15b34cc4dd9a8f0811d19e6725aafd33285908e
David Sampson <dsampson@mathworks.com>
2013-09-30 14:44:32 +0000

    Added documentation to uix.ancestors
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@465 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/ancestors.m
+++ code/+uix/ancestors.m
@@ -1,5 +1,14 @@
 function [a, f] = ancestors( h )
+%uix.ancestors  Get object ancestors
+%
+%  a = uix.ancestors(h) gets the ancestors of the object h, from top to
+%  bottom.  For rooted objects, the highest level ancestor returned is the
+%  figure, not the root.
+%
+%  [a,f] = uix.ancestors(h) returns the figure ancestor f directly.  For
+%  unrooted objects, f is an empty placeholder.
 
+% Find ancestors
 a = h; %  initialize
 p = h.Parent;
 while ~isempty( p ) && ~isa( p, 'matlab.ui.Root' )
@@ -7,6 +16,7 @@ while ~isempty( p ) && ~isa( p, 'matlab.ui.Root' )
     p = p.Parent;
 end
 
+% Return figure is requested
 if nargout > 1
     if isempty( p )
         f = p;
@@ -15,4 +25,4 @@ if nargout > 1
     end
 end
 
-end
+end % uix.ancestors
