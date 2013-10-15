1ae93c96c1b7b49826b4af30025ec8e3d0c5ef1e
David Sampson <dsampson@mathworks.com>
2013-10-15 21:15:13 +0000

    Added hboxflex
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@514 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/hboxflex.m
@@ -0,0 +1,14 @@
+function obj = hboxflex( varargin )
+
+% Check inputs
+uix.pvchk( varargin )
+
+% Construct
+obj = uix.HBoxFlex( varargin{:} );
+
+% Auto-parent
+if ~ismember( varargin(1:2:end), 'Parent' )
+    obj.Parent = gcf;
+end
+
+end % hboxflex
