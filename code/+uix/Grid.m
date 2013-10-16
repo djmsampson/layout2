8f475585d6b4ee0305c2a647b118ff5e49241f94
David Sampson <dsampson@mathworks.com>
2013-10-16 15:24:36 +0000

    Started implementing uix.Grid No support for legacy properties No support for resizing through changing widths and heights
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@525 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/+uix/Grid.m
@@ -0,0 +1,257 @@
+classdef Grid < uix.Box
+    
+    properties( Access = public, Dependent, AbortSet )
+        Widths % widths of contents, in pixels and/or weights
+        MinimumWidths % minimum widths of contents, in pixels
+        Heights % heights of contents, in pixels and/or weights
+        MinimumHeights % minimum heights of contents, in pixels
+    end
+    
+    properties( Access = protected )
+        Widths_ = zeros( [0 1] ) % backing for Widths
+        MinimumWidths_ = zeros( [0 1] ) % backing for MinimumWidths
+        Heights_ = zeros( [0 1] ) % backing for Heights
+        MinimumHeights_ = zeros( [0 1] ) % backing for MinimumHeights
+    end
+    
+    methods
+        
+        function obj = Grid( varargin )
+            
+            % Split input arguments
+            [mypv, notmypv] = uix.pvsplit( varargin, mfilename( 'class' ) );
+            
+            % Call superclass constructor
+            obj@uix.Box( notmypv{:} );
+            
+            % Set properties
+            if ~isempty( mypv )
+                set( obj, mypv{:} )
+            end
+            
+        end % constructor
+        
+    end % structors
+    
+    methods
+        
+        function value = get.Widths( obj )
+            
+            value = obj.Widths_;
+            
+        end % get.Widths
+        
+        function set.Widths( obj, value )
+            
+            % Check
+            assert( isa( value, 'double' ), 'uix:InvalidPropertyValue', ...
+                'Property ''Widths'' must be of type double.' )
+            assert( all( isreal( value ) ) && ~any( isinf( value ) ) && ...
+                ~any( isnan( value ) ), 'uix:InvalidPropertyValue', ...
+                'Elements of property ''Widths'' must be real and finite.' )
+            if isequal( size( value ), size( obj.Contents_ ) )
+                % OK
+            elseif isequal( size( value' ), size( obj.Contents_ ) )
+                % Warn and transpose
+                warning( 'uix:InvalidPropertyValue', ...
+                    'Size of property ''Widths'' must match size of contents.' )
+                value = value';
+            else
+                % Error
+                error( 'uix:InvalidPropertyValue', ...
+                    'Size of property ''Widths'' must match size of contents.' )
+            end
+            
+            % Set
+            obj.Widths_ = value;
+            
+            % Mark as dirty
+            obj.Dirty = true;
+            
+        end % set.Widths
+        
+        function value = get.MinimumWidths( obj )
+            
+            value = obj.MinimumWidths_;
+            
+        end % get.MinimumWidths
+        
+        function set.MinimumWidths( obj, value )
+            
+            % Check
+            assert( isa( value, 'double' ), 'uix:InvalidPropertyValue', ...
+                'Property ''MinimumWidths'' must be of type double.' )
+            assert( all( isreal( value ) ) && ~any( isinf( value ) ) && ...
+                all( value >= 0 ), 'uix:InvalidPropertyValue', ...
+                'Elements of property ''MinimumWidths'' must be non-negative.' )
+            if isequal( size( value ), size( obj.Contents_ ) )
+                % OK
+            elseif isequal( size( value' ), size( obj.Contents_ ) )
+                % Warn and transpose
+                warning( 'uix:InvalidPropertyValue', ...
+                    'Size of property ''MinimumWidths'' must match size of contents.' )
+                value = value';
+            else
+                % Error
+                error( 'uix:InvalidPropertyValue', ...
+                    'Size of property ''MinimumWidths'' must match size of contents.' )
+            end
+            
+            % Set
+            obj.MinimumWidths_ = value;
+            
+            % Mark as dirty
+            obj.Dirty = true;
+            
+        end % set.MinimumWidths
+        
+        function value = get.Heights( obj )
+            
+            value = obj.Heights_;
+            
+        end % get.Heights
+        
+        function set.Heights( obj, value )
+            
+            % Check
+            assert( isa( value, 'double' ), 'uix:InvalidPropertyValue', ...
+                'Property ''Heights'' must be of type double.' )
+            assert( all( isreal( value ) ) && ~any( isinf( value ) ) && ...
+                ~any( isnan( value ) ), 'uix:InvalidPropertyValue', ...
+                'Elements of property ''Heights'' must be real and finite.' )
+            if isequal( size( value ), size( obj.Contents_ ) )
+                % OK
+            elseif isequal( size( value' ), size( obj.Contents_ ) )
+                % Warn and transpose
+                warning( 'uix:InvalidPropertyValue', ...
+                    'Size of property ''Heights'' must match size of contents.' )
+                value = value';
+            else
+                % Error
+                error( 'uix:InvalidPropertyValue', ...
+                    'Size of property ''Heights'' must match size of contents.' )
+            end
+            
+            % Set
+            obj.Heights_ = value;
+            
+            % Mark as dirty
+            obj.Dirty = true;
+            
+        end % set.Heights
+        
+        function value = get.MinimumHeights( obj )
+            
+            value = obj.MinimumHeights_;
+            
+        end % get.MinimumHeights
+        
+        function set.MinimumHeights( obj, value )
+            
+            % Check
+            assert( isa( value, 'double' ), 'uix:InvalidPropertyValue', ...
+                'Property ''MinimumHeights'' must be of type double.' )
+            assert( all( isreal( value ) ) && ~any( isinf( value ) ) && ...
+                all( value >= 0 ), 'uix:InvalidPropertyValue', ...
+                'Elements of property ''MinimumHeights'' must be non-negative.' )
+            if isequal( size( value ), size( obj.Contents_ ) )
+                % OK
+            elseif isequal( size( value' ), size( obj.Contents_ ) )
+                % Warn and transpose
+                warning( 'uix:InvalidPropertyValue', ...
+                    'Size of property ''MinimumHeights'' must match size of contents.' )
+                value = value';
+            else
+                % Error
+                error( 'uix:InvalidPropertyValue', ...
+                    'Size of property ''MinimumHeights'' must match size of contents.' )
+            end
+            
+            % Set
+            obj.MinimumHeights_ = value;
+            
+            % Mark as dirty
+            obj.Dirty = true;
+            
+        end % set.MinimumHeights
+        
+    end % accessors
+    
+    methods( Access = protected )
+        
+        function redraw( obj )
+            
+            % Compute positions
+            bounds = hgconvertunits( ancestor( obj, 'figure' ), ...
+                obj.Position, obj.Units, 'pixels', obj.Parent );
+            widths = obj.Widths_;
+            minimumWidths = obj.MinimumWidths_;
+            heights = obj.Heights_;
+            minimumHeights = obj.MinimumHeights_;
+            padding = obj.Padding_;
+            spacing = obj.Spacing_;
+            nx = numel( widths );
+            ny = numel( heights );
+            n = numel( obj.Contents_ );
+            xSizes = uix.calcPixelSizes( bounds(3), widths, ...
+                minimumWidths, padding, spacing );
+            xPositions = [cumsum( [0; xSizes(1:end-1,:)] ) + padding + ...
+                spacing * transpose( 0:nx-1 ) + 1, xSizes];
+            ySizes = uix.calcPixelSizes( bounds(4), heights, ...
+                minimumHeights, padding, spacing );
+            yPositions = [bounds(4) - cumsum( ySizes ) - padding - ...
+                spacing * transpose( 0:ny-1 ) + 1, ySizes];
+            [ix, iy] = ind2sub( [nx ny], transpose( 1:n ) );
+            positions = [xPositions(ix,1), yPositions(iy,1), ...
+                xPositions(ix,2), yPositions(iy,2)];
+            
+            % Set positions
+            obj.reposition( positions );
+            
+        end % redraw
+        
+        function addChild( obj, child )
+            
+            % Add column if necessary
+            n = numel( obj.Contents_ );
+            nx = numel( obj.Widths_ );
+            ny = numel( obj.Heights_ );
+            if n == 0
+                obj.Widths_(end+1,:) = -1;
+                obj.MinimumWidths_(end+1,:) = 1;
+                obj.Heights_(end+1,:) = -1;
+                obj.MinimumHeights_(end+1,:) = 1;
+            elseif ceil( (n+1)/ny ) > nx
+                obj.Widths_(end+1,:) = -1;
+                obj.MinimumWidths_(end+1,:) = 1;
+            end
+            
+            % Call superclass method
+            addChild@uix.Box( obj, child )
+            
+        end % addChild
+        
+        function removeChild( obj, child )
+            
+            % Add column if necessary
+            n = numel( obj.Contents_ );
+            nx = numel( obj.Widths_ );
+            ny = numel( obj.Heights_ );
+            if n == 1
+                obj.Widths_(end,:) = [];
+                obj.MinimumWidths_(end,:) = [];
+                obj.Heights_(end,:) = [];
+                obj.MinimumHeights_(end,:) = [];
+            elseif ceil( (n-1)/ny ) < nx
+                obj.Widths_(end,:) = [];
+                obj.MinimumWidths_(end,:) = [];
+            end
+            
+            % Call superclass method
+            removeChild@uix.Box( obj, child )
+            
+        end % onChildRemoved
+        
+    end % template methods
+    
+end % classdef
