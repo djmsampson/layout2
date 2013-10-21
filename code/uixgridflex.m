09b50c05081cc0b7c970ea65a1db8ac70503b651
David Sampson <dsampson@mathworks.com>
2013-10-21 13:54:03 +0000

    Added uixgridflex
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@543 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/uixgridflex.m
@@ -0,0 +1,14 @@
+function obj = uixgridflex( varargin )
+
+% Check inputs
+uix.pvchk( varargin )
+
+% Construct
+obj = uix.GridFlex( varargin{:} );
+
+% Auto-parent
+if ~ismember( 'Parent', varargin(1:2:end) )
+    obj.Parent = gcf;
+end
+
+end % uixgridflex
