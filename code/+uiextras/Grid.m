d7d846375173d28ca01fb8af4114defeee968636
David Sampson <dsampson@mathworks.com>
2013-10-23 21:43:52 +0000

    Added uiextras.Grid, GridFlex
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@626 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/+uiextras/Grid.m
@@ -0,0 +1,103 @@
+classdef Grid < uix.Grid
+    %uiextras.Grid  Container with contents arranged in a grid
+    %
+    %   obj = uiextras.Grid() creates a new new grid layout with all
+    %   properties set to defaults. The number of rows and columns to use
+    %   is determined from the number of elements in the RowSizes and
+    %   ColumnSizes properties respectively. Child elements are arranged
+    %   down column one first, then column two etc. If there are
+    %   insufficient columns then a new one is added. The output is a new
+    %   layout object that can be used as the parent for other
+    %   user-interface components. The output is a new layout object that
+    %   can be used as the parent for other user-interface components.
+    %
+    %   obj = uiextras.Grid(param,value,...) also sets one or more
+    %   parameter values.
+    %
+    %   See the <a href="matlab:doc uiextras.Grid">documentation</a> for more detail and the list of properties.
+    %
+    %   Examples:
+    %   >> f = figure();
+    %   >> g = uiextras.Grid( 'Parent', f, 'Spacing', 5 );
+    %   >> uicontrol( 'Style', 'frame', 'Parent', g, 'Background', 'r' )
+    %   >> uicontrol( 'Style', 'frame', 'Parent', g, 'Background', 'b' )
+    %   >> uicontrol( 'Style', 'frame', 'Parent', g, 'Background', 'g' )
+    %   >> uiextras.Empty( 'Parent', g )
+    %   >> uicontrol( 'Style', 'frame', 'Parent', g, 'Background', 'c' )
+    %   >> uicontrol( 'Style', 'frame', 'Parent', g, 'Background', 'y' )
+    %   >> set( g, 'ColumnSizes', [-1 100 -2], 'RowSizes', [-1 100] );
+    %
+    %   See also: uiextras.GridFlex
+    
+    %   Copyright 2009-2013 The MathWorks, Inc.
+    %   $Revision: 367 $ $Date: 2011-02-10 16:25:22 +0000 (Thu, 10 Feb 2011) $
+    
+    properties( Hidden, Access = public, Dependent )
+        RowSizes % deprecated
+        ColumnSizes % deprecated
+    end
+    
+    methods
+        
+        function obj = Grid( varargin )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'uiextras.Grid is deprecated.  Please use uix.Grid instead.' )
+            
+            % Do
+            obj@uix.Grid( varargin{:} )
+            
+        end % constructor
+        
+    end % structors
+    
+    methods
+        
+        function value = get.RowSizes( obj )
+            
+            % Warn
+            warning( 'uix:Deprecated', ...
+                'Property ''RowSizes'' is deprecated.  Use ''Heights'' instead.' )
+            
+            % Get
+            value = transpose( obj.Widths );
+            
+        end % get.RowSizes
+        
+        function set.RowSizes( obj, value )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'Property ''RowSizes'' is deprecated.  Use ''Heights'' instead.' )
+            
+            % Set
+            obj.Widths = transpose( value );
+            
+        end % set.RowSizes
+        
+        function value = get.ColumnSizes( obj )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'Property ''ColumnSizes'' is deprecated.  Use ''Widths'' instead.' )
+            
+            % Get
+            value = transpose( obj.Widths );
+            
+        end % get.ColumnSizes
+        
+        function set.ColumnSizes( obj, value )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'Property ''ColumnSizes'' is deprecated.  Use ''Widths'' instead.' )
+            
+            % Get
+            obj.Widths = transpose( value );
+            
+        end % set.ColumnSizes
+        
+    end % accessors
+    
+end % classdef
---
+++ code/+uiextras/GridFlex.m
@@ -0,0 +1,106 @@
+classdef GridFlex < uix.GridFlex
+    %uiextras.GridFlex  Container with contents arranged in a resizable grid
+    %
+    %   obj = uiextras.GridFlex() creates a new new grid layout with
+    %   draggable dividers between elements. The number of rows and columns
+    %   to use is determined from the number of elements in the RowSizes
+    %   and ColumnSizes properties respectively. Child elements are
+    %   arranged down column one first, then column two etc. If there are
+    %   insufficient columns then a new one is added. The output is a new
+    %   layout object that can be used as the parent for other
+    %   user-interface components. The output is a new layout object that
+    %   can be used as the parent for other user-interface components.
+    %
+    %   obj = uiextras.GridFlex(param,value,...) also sets one or more
+    %   parameter values.
+    %
+    %   See the <a href="matlab:doc uiextras.GridFlex">documentation</a> for more detail and the list of properties.
+    %
+    %   Examples:
+    %   >> f = figure();
+    %   >> g = uiextras.GridFlex( 'Parent', f, 'Spacing', 5 );
+    %   >> uicontrol( 'Parent', g, 'Background', 'r' )
+    %   >> uicontrol( 'Parent', g, 'Background', 'b' )
+    %   >> uicontrol( 'Parent', g, 'Background', 'g' )
+    %   >> uiextras.Empty( 'Parent', g )
+    %   >> uicontrol( 'Parent', g, 'Background', 'c' )
+    %   >> uicontrol( 'Parent', g, 'Background', 'y' )
+    %   >> set( g, 'ColumnSizes', [-1 100 -2], 'RowSizes', [-1 -2] );
+    %
+    %   See also: uiextras.Grid
+    %             uiextras.HBoxFlex
+    %             uiextras.VBoxFlex
+    %             uiextras.Empty
+    
+    %   Copyright 2009-2013 The MathWorks, Inc.
+    %   $Revision: 366 $ $Date: 2011-02-10 15:48:11 +0000 (Thu, 10 Feb 2011) $
+    
+    properties( Hidden, Access = public, Dependent )
+        RowSizes % deprecated
+        ColumnSizes % deprecated
+    end
+    
+    methods
+        
+        function obj = GridFlex( varargin )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'uiextras.GridFlex is deprecated.  Please use uix.GridFlex instead.' )
+            
+            % Do
+            obj@uix.GridFlex( varargin{:} )
+            
+        end % constructor
+        
+    end % structors
+    
+    methods
+        
+        function value = get.RowSizes( obj )
+            
+            % Warn
+            warning( 'uix:Deprecated', ...
+                'Property ''RowSizes'' is deprecated.  Use ''Heights'' instead.' )
+            
+            % Get
+            value = transpose( obj.Widths );
+            
+        end % get.RowSizes
+        
+        function set.RowSizes( obj, value )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'Property ''RowSizes'' is deprecated.  Use ''Heights'' instead.' )
+            
+            % Set
+            obj.Widths = transpose( value );
+            
+        end % set.RowSizes
+        
+        function value = get.ColumnSizes( obj )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'Property ''ColumnSizes'' is deprecated.  Use ''Widths'' instead.' )
+            
+            % Get
+            value = transpose( obj.Widths );
+            
+        end % get.ColumnSizes
+        
+        function set.ColumnSizes( obj, value )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'Property ''ColumnSizes'' is deprecated.  Use ''Widths'' instead.' )
+            
+            % Get
+            obj.Widths = transpose( value );
+            
+        end % set.ColumnSizes
+        
+    end % accessors
+    
+end % classdef
