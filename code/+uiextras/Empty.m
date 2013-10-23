4be82af81fec265b57c7e5a077e9749b9094ab8f
David Sampson <dsampson@mathworks.com>
2013-10-23 21:13:01 +0000

    Implemented uiextras.Empty
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@623 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uiextras/Empty.m
+++ code/+uiextras/Empty.m
@@ -1,11 +1,11 @@
-function obj = Empty()
+function obj = Empty( varargin )
 %uiextras.Empty  Create an empty space
 %
-%   obj = uiextras.Empty() creates an empty space object that can be
-%   used in layouts to add gaps between other elements.
+%   obj = uiextras.Empty() creates a placeholder that can be used to add
+%   gaps between elements in layouts.
 %
-%   obj = uiextras.Empty(param,value,...) also sets one or more
-%   property values.
+%   obj = uiextras.Empty(param,value,...) also sets one or more property
+%   values.
 %
 %   See the <a href="matlab:doc uiextras.Empty">documentation</a> for more detail and the list of properties.
 %
@@ -15,18 +15,15 @@ function obj = Empty()
 %   >> uicontrol( 'Parent', box, 'Background', 'r' )
 %   >> uiextras.Empty( 'Parent', box )
 %   >> uicontrol( 'Parent', box, 'Background', 'b' )
-%
-%   See also: uiextras.HBox
 
-%   Copyright 2009-2010 The MathWorks, Inc.
-%   $Revision: 287 $
-%   $Date: 2010-07-14 12:21:33 +0100 (Wed, 14 Jul 2010) $
+%   Copyright 2009-2013 The MathWorks, Inc.
+%   $Revision: 287 $ $Date: 2010-07-14 12:21:33 +0100 (Wed, 14 Jul 2010) $
 
 % Warn
-warning( 'uiextras:Deprecated', ...
-    'uiextras.Empty is deprecated.  Please use gobjects instead.' )
+warning( 'uix:Deprecated', ...
+    'uiextras.Empty is deprecated.  Please use uicontainer instead.' )
 
 % Do
-obj = gobjects( 1 );
+obj = matlab.ui.container.internal.UIContainer( varargin{:} );
 
 end % uiextras.Empty
