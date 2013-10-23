14923be01cc1c4df1a9c01fa7b2c60d692292550
David Sampson <dsampson@mathworks.com>
2013-10-23 22:31:32 +0000

    Added uiextras.Panel
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@635 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/+uiextras/Panel.m
@@ -0,0 +1,80 @@
+classdef Panel < matlab.ui.container.Panel
+    %uiextras.Panel  Show one element inside a panel
+    %
+    %   obj = uiextras.Panel() creates a standard UIPANEL object but with
+    %   automatic management of the contained widget or layout. The
+    %   properties available are largely the same as the builtin UIPANEL
+    %   object. Where more than one child is added, the currently visible
+    %   child is determined using the SelectedChild property.
+    %
+    %   obj = uiextras.Panel(param,value,...) also sets one or more
+    %   property values.
+    %
+    %   See the <a href="matlab:doc uiextras.Panel">documentation</a> for more detail and the list of properties.
+    %
+    %   Examples:
+    %   >> f = figure();
+    %   >> p = uiextras.Panel( 'Parent', f, 'Title', 'A Panel', 'Padding', 5 );
+    %   >> uicontrol( 'Parent', p, 'Background', 'r' )
+    %
+    %   >> f = figure();
+    %   >> p = uiextras.Panel( 'Parent', f, 'Title', 'A Panel', 'Padding', 5 );
+    %   >> b = uiextras.HBox( 'Parent', p, 'Spacing', 5 );
+    %   >> uicontrol( 'Style', 'listbox', 'Parent', b, 'String', {'Item 1','Item 2'} );
+    %   >> uicontrol( 'Parent', b, 'Background', 'b' );
+    %   >> set( b, 'Sizes', [100 -1] );
+    %
+    %   See also: uipanel
+    %             uiextras.BoxPanel
+    %             uiextras.HBox
+    
+    %   Copyright 2009-2013 The MathWorks, Inc.
+    %   $Revision: 373 $ $Date: 2011-07-14 13:24:10 +0100 (Thu, 14 Jul 2011) $
+    
+    properties( Hidden, Access = public, Dependent )
+        SelectedChild % deprecated
+    end
+    
+    methods
+        
+        function obj = Panel( varargin )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'uiextras.Panel is deprecated.  Please use matlab.ui.container.Panel instead.' )
+            
+            % Do
+            obj@matlab.ui.container.Panel( varargin{:} )
+            
+        end % constructor
+        
+    end % structors
+    
+    methods
+        
+        function value = get.SelectedChild( obj )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'Property ''SelectedChild'' is deprecated.' )
+            
+            % Get
+            if isempty( obj.Contents_ )
+                value = [];
+            else
+                value = 1;
+            end
+            
+        end % get.SelectedChild
+        
+        function set.SelectedChild( ~, ~ )
+            
+            % Warn
+            warning( 'uiextras:Deprecated', ...
+                'Property ''MinimumSizes'' is deprecated.  Use ''MinimumHeights'' instead.' )
+            
+        end % set.SelectedChild
+        
+    end % accessors
+    
+end % classdef
