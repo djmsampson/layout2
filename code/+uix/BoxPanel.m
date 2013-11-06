aceacf3e9bcf77a575764bd158e53ffc5983f324
David Sampson <dsampson@mathworks.com>
2013-11-06 12:38:24 +0000

    Reverted JLabel changes
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@697 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/BoxPanel.m
+++ code/+uix/BoxPanel.m
@@ -4,31 +4,12 @@ classdef BoxPanel < uix.Container
         Title
         BorderWidth
         BorderType
-        FontAngle
-        FontName
-        FontSize
-        FontWeight
-        FontUnits
-        ForegroundColor
-        HighlightColor
-        ShadowColor
     end
     
-    properties % ( Access = private )
-        TitleLabel
-        TitleContainer
+    properties( Access = private )
+        Titlebar
         BorderWidth_ = 0
         BorderType_ = 'none'
-        FontUnits_ = 'points'
-        HilightColor_ = [1 1 1]
-        ShadowColor_ = [0.7 0.7 0.7]
-    end
-    
-    properties( Access = private, Constant )
-        DefaultFontName = get( 0, 'DefaultUicontrolFontName' )
-        DefaultFontSize = get( 0, 'DefaultUicontrolFontSize' )
-        DefaultFontUnits = get( 0, 'DefaultUicontrolFontUnits' )
-        ScreenPixelsPerInch = get( 0, 'ScreenPixelsPerInch' )
     end
     
     methods
@@ -39,19 +20,12 @@ classdef BoxPanel < uix.Container
             obj@uix.Container()
             
             % Create title
-            titleLabel = javaObjectEDT( 'javax.swing.JLabel' );
-            titleContainer = hgjavacomponent( 'Internal', true, ...
-                'Parent', obj, 'JavaPeer', titleLabel );
-            titleContainer.Units = 'pixels';
+            titlebar = matlab.ui.control.StyleControl( 'Internal', true, ...
+                'Parent', obj, 'Style', 'text', 'Units', 'pixels' );
+            titlebar.BackgroundColor = [0 0 1];
             
             % Store properties
-            obj.TitleLabel = titleLabel;
-            obj.TitleContainer = titleContainer;
-            
-            % Apply defaults
-            obj.FontName = obj.DefaultFontName;
-            obj.FontUnits = obj.DefaultFontUnits;
-            obj.FontSize = obj.DefaultFontSize;
+            obj.Titlebar = titlebar;
             
             % Set properties
             if nargin > 0
@@ -95,186 +69,15 @@ classdef BoxPanel < uix.Container
             
         end % set.BorderType
         
-        function value = get.FontAngle( obj )
-            
-            if obj.TitleLabel.getFont().isItalic()
-                value = 'italic';
-            else
-                value = 'normal';
-            end
-            
-        end % get.FontAngle
-        
-        function set.FontAngle( obj, value ) % TODO
-            
-            % Check
-            assert( any( strcmp( value, {'normal','italic','oblique'} ) ), ...
-                'uix:InvalidPropertyValue', ...
-                'Property ''FontAngle'' must be ''normal'', ''italic'' or ''oblique''.' )
-            
-            % Set font
-            titleLabel = obj.TitleLabel;
-            oldFont = titleLabel.getFont();
-            newStyle = 2 * ~strcmp( value, 'normal' ) + oldFont.isBold();
-            newFont = java.awt.Font( oldFont.getName(), newStyle, oldFont.getSize2D() );
-            titleLabel.setFont( newFont );
-            
-            % Set as dirty
-            obj.Dirty = true;
-            
-        end % set.FontAngle
-        
-        function value = get.FontName( obj )
-            
-            value = obj.TitleLabel.getFont().getFontName();
-            
-        end % get.FontName
-        
-        function set.FontName( obj, value )
-            
-            % Check
-            assert( ischar( value ), 'uix:InvalidPropertyValue', ...
-                'Property ''FontName'' must be a string.' )
-            
-            % Set
-            titleLabel = obj.TitleLabel;
-            oldFont = titleLabel.getFont();
-            newFont = java.awt.Font( value, oldFont.getStyle(), oldFont.getSize2D() );
-            titleLabel.setFont( newFont );
-            
-            % Set as dirty
-            obj.Dirty = true;
-            
-        end % set.FontName
-        
-        function value = get.FontSize( obj )
-            
-            pixels = obj.TitleLabel.getFont().getSize2D();
-            switch obj.FontUnits_
-                case 'inches'
-                    value = pixels / obj.ScreenPixelsPerInch;
-                case 'centimeters'
-                    value = pixels / obj.ScreenPixelsPerInch * 2.54;
-                case 'points'
-                    value = pixels * 72 / obj.ScreenPixelsPerInch;
-                case 'pixels'
-                    value = pixels;
-            end
-            
-        end % get.FontSize
-        
-        function set.FontSize( obj, value )
-            
-            % Check
-            assert( isa( value, 'double' ) && isscalar( value ) && ...
-                isreal( value ) && ~isinf( value ) && ...
-                ~isnan( value ) && value >= 0, ...
-                'uix:InvalidPropertyValue', ...
-                'Property ''FontSize'' must be a non-negative scalar.' )
-            
-            % Set
-            switch obj.FontUnits_
-                case 'inches'
-                    pixels = value * obj.ScreenPixelsPerInch;
-                case 'centimeters'
-                    pixels = value * obj.ScreenPixelsPerInch / 2.54;
-                case 'points'
-                    pixels = value * obj.ScreenPixelsPerInch / 72;
-                case 'pixels'
-                    pixels = value;
-            end
-            titleLabel = obj.TitleLabel;
-            oldFont = titleLabel.getFont();
-            newFont = java.awt.Font( oldFont.getName(), oldFont.getStyle(), pixels );
-            titleLabel.setFont( newFont );
-            
-            % Set as dirty
-            obj.Dirty = true;
-            
-        end % set.FontSize
-        
-        function value = get.FontUnits( obj )
-            
-            value = obj.FontUnits_;
-            
-        end % get.FontUnits
-        
-        function set.FontUnits( obj, value )
-            
-            % Check
-            assert( any( strcmp( value, {'inches','centimeters','points','pixels'} ) ), ...
-                'uix:InvalidPropertyValue', ...
-                'Property ''FontUnits'' must be ''inches'', ''centimeters'', ''points'' or ''pixels''.' )
-            
-            % Set
-            obj.FontUnits_ = value;
-            
-        end % set.FontUnits
-        
-        function value = get.FontWeight( obj )
-            
-            if obj.TitleLabel.getFont().isBold()
-                value = 'bold';
-            else
-                value = 'normal';
-            end
-            
-        end % get.FontWeight
-        
-        function set.FontWeight( obj, value ) % TODO
-            
-            obj.TitleContainer.FontWeight = value;
-            
-            % Set as dirty
-            obj.Dirty = true;
-            
-        end % set.FontWeight
-        
-        function value = get.ForegroundColor( obj )
-            
-            value = obj.TitleContainer.ForegroundColor;
-            
-        end % get.ForegroundColor
-        
-        function set.ForegroundColor( obj, value )
-            
-            obj.TitleContainer.ForegroundColor = value;
-            
-        end % set.ForegroundColor
-        
-        function value = get.HighlightColor( obj )
-            
-            value = obj.HighlightColor_;
-            
-        end % get.HighlightColor
-        
-        function set.HighlightColor( obj, value )
-            
-            obj.HighlightColor_ = value;
-            
-        end % set.HighlightColor
-        
-        function value = get.ShadowColor( obj )
-            
-            value = obj.ShadowColor_;
-            
-        end % get.ShadowColor
-        
-        function set.ShadowColor( obj, value )
-            
-            obj.ShadowColor_ = value;
-            
-        end % set.ShadowColor
-        
         function value = get.Title( obj )
             
-            value = char( obj.TitleLabel.getText() );
+            value = obj.Titlebar.String;
             
         end % get.Title
         
         function set.Title( obj, value )
             
-            obj.TitleLabel.setText( value )
+            obj.Titlebar.String = value;
             
             % Set as dirty
             obj.Dirty = true;
@@ -288,10 +91,9 @@ classdef BoxPanel < uix.Container
         function redraw( obj )
             
             % Compute positions
-            figure = ancestor( obj, 'figure' );
-            bounds = hgconvertunits( figure, [0 0 1 1], 'normalized', ...
-                'pixels', obj );
-            fontHeight = obj.TitleLabel.getFont().getSize2D() / 72 * obj.ScreenPixelsPerInch;
+            bounds = hgconvertunits( ancestor( obj, 'figure' ), ...
+                [0 0 1 1], 'normalized', 'pixels', obj );
+            extent = obj.Titlebar.Extent;
             switch obj.BorderType_
                 case 'none'
                     borderSize = 0;
@@ -303,16 +105,16 @@ classdef BoxPanel < uix.Container
             padding = obj.Padding_;
             xSizes = uix.calcPixelSizes( bounds(3), -1, ...
                 2 * padding + 1, borderSize, borderSize );
-            ySizes = uix.calcPixelSizes( bounds(4), [fontHeight; -1], ...
-                [fontHeight; 1 + 2 * padding], borderSize, borderSize );
-            titlePosition = [1 + borderSize, 1 + 2 * borderSize + ySizes(2), ...
+            ySizes = uix.calcPixelSizes( bounds(4), [extent(4); -1], ...
+                [extent(4); 1 + 2 * padding], borderSize, borderSize );
+            titlePosition = [borderSize, 2 * borderSize + ySizes(2), ...
                 xSizes, ySizes(1)];
-            contentsPosition = [1 + borderSize + padding, ...
-                1 + borderSize + padding, xSizes - 2 * padding, ...
+            contentsPosition = [borderSize + padding, ...
+                borderSize + padding, xSizes - 2 * padding, ...
                 ySizes(2) - 2 * padding];
             
             % Set decorations positions
-            obj.TitleContainer.Position = titlePosition;
+            obj.Titlebar.Position = titlePosition;
             
             % Set positions and visibility
             children = obj.Contents_;
