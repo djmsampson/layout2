492f0563b31044d7686b1a2bb9f4dad5746640f2
David Sampson <dsampson@mathworks.com>
2013-11-06 09:34:23 +0000

    Removed uix.javacomponent
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@692 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/+uix/BoxPanel.m
+++ code/+uix/BoxPanel.m
@@ -4,12 +4,23 @@ classdef BoxPanel < uix.Container
         Title
         BorderWidth
         BorderType
+        FontAngle
+        FontName
+        FontSize
+        FontWeight
+        FontUnits
+        ForegroundColor
+        HighlightColor
+        ShadowColor
     end
     
-    properties( Access = private )
+    properties % ( Access = private )
+        JTitlebar
         Titlebar
         BorderWidth_ = 0
         BorderType_ = 'none'
+        HilightColor_ = [1 1 1]
+        ShadowColor_ = [0.7 0.7 0.7]
     end
     
     methods
@@ -20,11 +31,13 @@ classdef BoxPanel < uix.Container
             obj@uix.Container()
             
             % Create title
-            titlebar = matlab.ui.control.StyleControl( 'Internal', true, ...
-                'Parent', obj, 'Style', 'text', 'Units', 'pixels' );
-            titlebar.BackgroundColor = [0 0 1];
+            jTitlebar = javax.swing.JLabel();
+            titlebar = hgjavacomponent( 'Internal', true, ...
+                'Parent', obj, 'JavaPeer', jTitlebar );
+            titlebar.Units = 'pixels';
             
             % Store properties
+            obj.JTitlebar = jTitlebar;
             obj.Titlebar = titlebar;
             
             % Set properties
@@ -69,15 +82,126 @@ classdef BoxPanel < uix.Container
             
         end % set.BorderType
         
+        function value = get.FontAngle( obj ) % TODO
+            
+            value = obj.Titlebar.FontAngle;
+            
+        end % get.FontAngle
+        
+        function set.FontAngle( obj, value ) % TODO
+            
+            obj.Titlebar.FontAngle = value;
+            
+            % Set as dirty
+            obj.Dirty = true;
+            
+        end % set.FontAngle
+        
+        function value = get.FontName( obj )
+            
+            value = obj.JTitlebar.getFont().getFontName();
+            
+        end % get.FontName
+        
+        function set.FontName( obj, value ) % TODO
+            
+            obj.Titlebar.FontName = value;
+            
+            % Set as dirty
+            obj.Dirty = true;
+            
+        end % set.FontName
+        
+        function value = get.FontSize( obj ) % TODO
+            
+            value = obj.JTitlebar.getFont().getSize2D();
+            
+        end % get.FontSize
+        
+        function set.FontSize( obj, value ) % TODO
+            
+            obj.Titlebar.FontSize = value;
+            
+            % Set as dirty
+            obj.Dirty = true;
+            
+        end % set.FontSize
+        
+        function value = get.FontUnits( obj ) % TODO
+            
+            value = obj.Titlebar.FontUnits;
+            
+        end % get.FontUnits
+        
+        function set.FontUnits( obj, value ) % TODO
+            
+            obj.Titlebar.FontUnits = value;
+            
+            % Set as dirty
+            obj.Dirty = true;
+            
+        end % set.FontUnits
+        
+        function value = get.FontWeight( obj ) % TODO
+            
+            value = obj.Titlebar.FontWeight;
+            
+        end % get.FontWeight
+        
+        function set.FontWeight( obj, value ) % TODO
+            
+            obj.Titlebar.FontWeight = value;
+            
+            % Set as dirty
+            obj.Dirty = true;
+            
+        end % set.FontWeight
+        
+        function value = get.ForegroundColor( obj )
+            
+            value = obj.Titlebar.ForegroundColor;
+            
+        end % get.ForegroundColor
+        
+        function set.ForegroundColor( obj, value )
+            
+            obj.Titlebar.ForegroundColor = value;
+            
+        end % set.ForegroundColor
+        
+        function value = get.HighlightColor( obj )
+            
+            value = obj.HighlightColor_;
+            
+        end % get.HighlightColor
+        
+        function set.HighlightColor( obj, value )
+            
+            obj.HighlightColor_ = value;
+            
+        end % set.HighlightColor
+        
+        function value = get.ShadowColor( obj )
+            
+            value = obj.ShadowColor_;
+            
+        end % get.ShadowColor
+        
+        function set.ShadowColor( obj, value )
+            
+            obj.ShadowColor_ = value;
+            
+        end % set.ShadowColor
+        
         function value = get.Title( obj )
             
-            value = obj.Titlebar.String;
+            value = char( obj.JTitlebar.getText() );
             
         end % get.Title
         
         function set.Title( obj, value )
             
-            obj.Titlebar.String = value;
+            obj.JTitlebar.setText( value )
             
             % Set as dirty
             obj.Dirty = true;
@@ -91,9 +215,10 @@ classdef BoxPanel < uix.Container
         function redraw( obj )
             
             % Compute positions
-            bounds = hgconvertunits( ancestor( obj, 'figure' ), ...
-                [0 0 1 1], 'normalized', 'pixels', obj );
-            extent = obj.Titlebar.Extent;
+            figure = ancestor( obj, 'figure' );
+            bounds = hgconvertunits( figure, [0 0 1 1], 'normalized', ...
+                'pixels', obj );
+            fontHeight = 20;
             switch obj.BorderType_
                 case 'none'
                     borderSize = 0;
@@ -105,12 +230,12 @@ classdef BoxPanel < uix.Container
             padding = obj.Padding_;
             xSizes = uix.calcPixelSizes( bounds(3), -1, ...
                 2 * padding + 1, borderSize, borderSize );
-            ySizes = uix.calcPixelSizes( bounds(4), [extent(4); -1], ...
-                [extent(4); 1 + 2 * padding], borderSize, borderSize );
-            titlePosition = [borderSize, 2 * borderSize + ySizes(2), ...
+            ySizes = uix.calcPixelSizes( bounds(4), [fontHeight; -1], ...
+                [fontHeight; 1 + 2 * padding], borderSize, borderSize );
+            titlePosition = [1 + borderSize, 1 + 2 * borderSize + ySizes(2), ...
                 xSizes, ySizes(1)];
-            contentsPosition = [borderSize + padding, ...
-                borderSize + padding, xSizes - 2 * padding, ...
+            contentsPosition = [1 + borderSize + padding, ...
+                1 + borderSize + padding, xSizes - 2 * padding, ...
                 ySizes(2) - 2 * padding];
             
             % Set decorations positions
