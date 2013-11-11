ea2354b8c2cb213a2c6cdc2a8bd3ab0671ce69c5
David Sampson <dsampson@mathworks.com>
2013-11-11 08:58:40 +0000

    Deprecated layoutRoot
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@728 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/uninstall.m
+++ code/uninstall.m
@@ -8,7 +8,7 @@ function uninstall()
 %  Copyright 2009-2013 The MathWorks, Inc.
 %  $Revision: 383 $ $Date: 2013-04-29 11:44:48 +0100 (Mon, 29 Apr 2013) $
 
-root = layoutRoot();
+root = fileparts( mfilename( 'fullpath' ) ); % this directory
 rmpath( root )
 rmpath( fullfile( root, 'layout' ) )
 savepath()
