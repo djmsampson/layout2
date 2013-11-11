b8a162d711b8d3242dce60f5b5b2abeb73be7ac1
David Sampson <dsampson@mathworks.com>
2013-11-11 08:56:29 +0000

    Added uixtabpanel
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@727 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/uixtabpanel.m
@@ -0,0 +1,14 @@
+function obj = uixtabpanel( varargin )
+
+% Check inputs
+uix.pvchk( varargin )
+
+% Construct
+obj = uix.TabPanel( varargin{:} );
+
+% Auto-parent
+if ~ismember( 'Parent', varargin(1:2:end) )
+    obj.Parent = gcf;
+end
+
+end % uixtabpanel
