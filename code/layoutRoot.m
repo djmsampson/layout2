69d38b35e793828b9f615d98e98a79e72f312c1d
David Sampson <dsampson@mathworks.com>
2013-11-11 09:02:07 +0000

    Added layoutRoot
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@729 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/layoutRoot.m
@@ -0,0 +1,18 @@
+function folder = layoutRoot()
+%layoutRoot  Folder containing the GUI Layout Toolbox
+%
+%   folder = layoutRoot() returns the full path to the folder containing
+%   the GUI Layout Toolbox.
+%
+%   Examples:
+%   >> folder = layoutRoot()
+%   folder = 'C:\tools\layouts2'
+%
+%   See also: layoutVersion
+
+%  Copyright 2009-2013 The MathWorks, Inc.
+%  $Revision: 383 $ $Date: 2013-04-29 11:44:48 +0100 (Mon, 29 Apr 2013) $
+
+folder = fileparts( mfilename( 'fullpath' ) );
+
+end % layoutRoot
