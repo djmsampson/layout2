594156c739a0ffafb16ed05c6c44e2b32e8f5f49
David Sampson <dsampson@mathworks.com>
2013-11-13 06:47:30 +0000

    Boarded up uiextras.get, set, unset
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@778 4f12366f-9e95-4241-96af-8f93ba654eeb

---
+++ code/+uiextras/get.m
@@ -0,0 +1,15 @@
+function varargout = get( ~, ~ ) %#ok<STOUT>
+%uiextras.get  Retrieve a default property value from a parent object
+%
+%  This functionality has been removed.
+
+%  Copyright 2009-2013 The MathWorks, Inc.
+%  $Revision: 383 $ $Date: 2013-04-29 11:44:48 +0100 (Mon, 29 Apr 2013) $
+
+% Check inputs
+narginchk( 2, 2 )
+
+% Error
+error( 'uiextras:Deprecated', 'uiextras.get has been removed.' )
+
+end % uiextras.get
