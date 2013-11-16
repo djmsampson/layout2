0add2cdf00053e404a06d11881eb29c5aa6277fa
David Sampson <dsampson@mathworks.com>
2013-11-16 15:36:10 +0000

    Added copyrights and H1 lines to public functions
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@848 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/layout/layoutVersion.m
+++ code/layout/layoutVersion.m
@@ -1,19 +1,20 @@
 function [version, versionDate] = layoutVersion()
 %layoutVersion  get the toolbox version and date
 %
-%   v = layoutVersion() returns just the version string
+%  v = layoutVersion() returns just the version string
 %
-%   [v,d] = layoutVersion() returns both the version string and the date of
-%   creation.  The date format is ISO8601, YYYY-MM-DD.
+%  [v,d] = layoutVersion() returns both the version string and the date of
+%  creation.  The date format is ISO8601, YYYY-MM-DD.
 %
-%   Examples:
-%   >> [v,d] = layoutVersion()
-%   v = '1.0'
-%   d = '2010-05-28'
+%  Examples:
+%  >> [v,d] = layoutVersion()
+%  v = '1.0'
+%  d = '2010-05-28'
 %
-%   See also: layoutRoot
+%  See also: layoutRoot
 
-%   Copyright 2009-2013 The MathWorks Ltd.
+%  Copyright 2009-2013 The MathWorks, Inc.
+%  $Revision: 383 $ $Date: 2013-04-29 11:44:48 +0100 (Mon, 29 Apr 2013) $
 
 % Issue warning
 warning( 'uiextras:Deprecated', 'layoutVersion will be removed in a future release.  Please use ver(''layout'') instead.' )
