bda55e3c56d7eec7ce71a201a930f517fae48bde
David Sampson <dsampson@mathworks.com>
2013-12-03 17:53:41 +0000

    Improved sophistication of install, uninstall so that temporary changes to the path are not saved
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@891 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/layout/install.m
+++ code/layout/install.m
@@ -9,8 +9,28 @@ function install()
 %  Copyright 2009-2013 The MathWorks, Inc.
 %  $Revision$ $Date$
 
-root = fileparts( mfilename( 'fullpath' ) ); % this directory
-addpath( root )
+% Capture path
+oldPathList = path();
+
+% Add toolbox directory to saved path
+userPathList = userpath();
+if isempty( userPathList )
+    userPathCell = cell( [0 1] );
+else
+    userPathCell = textscan( userPathList, '%s', 'Delimiter', ';' );
+    userPathCell = userPathCell{:};
+end
+savedPathList = pathdef();
+savedPathCell = textscan( savedPathList, '%s', 'Delimiter', ';' );
+savedPathCell = savedPathCell{:};
+savedPathCell = setdiff( savedPathCell, userPathCell, 'stable' );
+toolPathCell = {fileparts( mfilename( 'fullpath' ) )}; % this directory
+savedPathCell = [toolPathCell; savedPathCell];
+path( sprintf( '%s;', userPathCell{:}, savedPathCell{:} ) )
 savepath()
+
+% Restore path plus toolbox directory
+path( oldPathList )
+addpath( sprintf( '%s;', toolPathCell{:} ) )
 
 end % install
--- code/layout/uninstall.m
+++ code/layout/uninstall.m
@@ -9,8 +9,28 @@ function uninstall()
 %  Copyright 2009-2013 The MathWorks, Inc.
 %  $Revision$ $Date$
 
-root = fileparts( mfilename( 'fullpath' ) ); % this directory
-rmpath( root )
+% Capture path
+oldPathList = path();
+
+% Remove toolbox directory from saved path
+userPathList = userpath();
+if isempty( userPathList )
+    userPathCell = cell( [0 1] );
+else
+    userPathCell = textscan( userPathList, '%s', 'Delimiter', ';' );
+    userPathCell = userPathCell{:};
+end
+savedPathList = pathdef();
+savedPathCell = textscan( savedPathList, '%s', 'Delimiter', ';' );
+savedPathCell = savedPathCell{:};
+savedPathCell = setdiff( savedPathCell, userPathCell, 'stable' );
+toolPathCell = {fileparts( mfilename( 'fullpath' ) )}; % this directory
+savedPathCell = setdiff( savedPathCell, toolPathCell, 'stable' );
+path( sprintf( '%s;', userPathCell{:}, savedPathCell{:} ) )
 savepath()
+
+% Restore path minus toolbox directory
+path( oldPathList )
+rmpath( sprintf( '%s;', toolPathCell{:} ) )
 
 end % uninstall
