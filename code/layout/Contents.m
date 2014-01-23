6f7e1ed3e1217552969443dbee0e600cbd7b77b7
David Sampson <dsampson@mathworks.com>
2014-01-23 02:13:43 +0000

    Removed construction functions
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@897 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/layout/Contents.m
+++ code/layout/Contents.m
@@ -1,23 +1,23 @@
 % GUI Layout Toolbox
-% Version 2.0.3 (R2013b+) 27-Nov-2013
+% Version 2.0.4 (R2013b+) 22-Jan-2014
 %
 % Panels
-%   uixpanel      - arrange a single element inside a standard panel
-%   uixcardpanel  - show one element from a list
-%   uixboxpanel   - arrange a single element inside a panel with a boxed title
-%   uixtabpanel   - arrange elements in a panel with tabs for selecting which is visible
+%   uix.Panel      - arrange a single element inside a standard panel
+%   uix.CardPanel  - show one element from a list
+%   uix.BoxPanel   - arrange a single element inside a panel with a boxed title
+%   uix.TabPanel   - arrange elements in a panel with tabs for selecting which is visible
 %
 % Boxes
-%   uixhbox       - arrange elements horizontally in a single row
-%   uixvbox       - arrange elements vertically in a single column
-%   uixhboxflex   - arrange elements horizontally with draggable dividers
-%   uixvboxflex   - arrange elements vertically with draggable dividers
-%   uixhbuttonbox - arrange buttons horizontally in a single row
-%   uixvbuttonbox - arrange buttons vertically in a single column
+%   uix.HBox       - arrange elements horizontally in a single row
+%   uix.VBox       - arrange elements vertically in a single column
+%   uix.HBoxFlex   - arrange elements horizontally with draggable dividers
+%   uix.VBoxFlex   - arrange elements vertically with draggable dividers
+%   uix.HButtonBox - arrange buttons horizontally in a single row
+%   uix.VButtonBox - arrange buttons vertically in a single column
 %
 % Grids
-%   uixgrid       - arrange elements in a two-dimensional grid
-%   uixgridflex   - arrange elements in a two-dimensional grid with draggable dividers
+%   uix.Grid       - arrange elements in a two-dimensional grid
+%   uix.GridFlex   - arrange elements in a two-dimensional grid with draggable dividers
 %
 % Installation
 %   install       - install toolbox
@@ -26,5 +26,5 @@
 % See also
 %   <a href="readme.txt">Release notes</a>
 
-%  Copyright 2009-2013 The MathWorks, Inc.
+%  Copyright 2009-2014 The MathWorks, Inc.
 %  $Revision$ $Date$
