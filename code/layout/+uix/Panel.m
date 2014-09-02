6a067df643e01ff951895f23818047fc6bd865c5
David Sampson <dsampson@mathworks.com>
2014-09-02 10:53:37 +0000

    Added cardlike behavior to uix.Panel
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@953 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/layout/+uix/Panel.m
+++ code/layout/+uix/Panel.m
@@ -1,12 +1,19 @@
 classdef Panel < matlab.ui.container.Panel & uix.mixin.Container
     
+    properties( Access = public, Dependent, AbortSet )
+        Selection % selected contents
+    end
+    
+    properties( Access = protected )
+        Selection_ = 0 % backing for Selection
+    end
+    
     methods
         
         function obj = Panel( varargin )
             
-            % Call superclass constructors
+            % Call superclass constructor
             obj@matlab.ui.container.Panel()
-            obj@uix.mixin.Container()
             
             % Set properties
             if nargin > 0
@@ -17,6 +24,44 @@ classdef Panel < matlab.ui.container.Panel & uix.mixin.Container
         end % constructor
         
     end % structors
+    
+    methods
+        
+        function value = get.Selection( obj )
+            
+            value = obj.Selection_;
+            
+        end % get.Selection
+        
+        function set.Selection( obj, value )
+            
+            % Check
+            assert( isa( value, 'double' ), 'uix:InvalidPropertyValue', ...
+                'Property ''Selection'' must be of type double.' )
+            assert( isequal( size( value ), [1 1] ), ...
+                'uix:InvalidPropertyValue', ...
+                'Property ''Selection'' must be scalar.' )
+            assert( isreal( value ) && rem( value, 1 ) == 0, ...
+                'uix:InvalidPropertyValue', ...
+                'Property ''Selection'' must be an integer.' )
+            n = numel( obj.Contents_ );
+            if n == 0
+                assert( value == 0, 'uix:InvalidPropertyValue', ...
+                    'Property ''Selection'' must be 0 for a container with no children.' )
+            else
+                assert( value >= 1 && value <= n, 'uix:InvalidPropertyValue', ...
+                    'Property ''Selection'' must be between 1 and the number of children.' )
+            end
+            
+            % Set
+            obj.Selection_ = value;
+            
+            % Mark as dirty
+            obj.Dirty = true;
+            
+        end % set.Selection
+        
+    end % accessors
     
     methods( Access = protected )
         
@@ -32,8 +77,8 @@ classdef Panel < matlab.ui.container.Panel & uix.mixin.Container
             
             % Set positions and visibility
             children = obj.Contents_;
-            selection = numel( children );
-            for ii = 1:selection
+            selection = obj.Selection_;
+            for ii = 1:numel( children )
                 child = children(ii);
                 if ii == selection
                     child.Visible = 'on';
@@ -62,6 +107,53 @@ classdef Panel < matlab.ui.container.Panel & uix.mixin.Container
             end
             
         end % redraw
+        
+        function addChild( obj, child )
+            
+            % Select new content
+            obj.Selection_ = numel( obj.Contents_ ) + 1;
+            
+            % Call superclass method
+            addChild@uix.mixin.Container( obj, child )
+            
+        end % addChild
+        
+        function removeChild( obj, child )
+            
+            % Adjust selection if required
+            contents = obj.Contents_;
+            n = numel( contents );
+            index = find( contents == child );
+            selection = obj.Selection_;
+            if index == 1 && selection == 1 && n > 1
+                % retain selection
+            elseif index <= selection
+                obj.Selection_ = selection - 1;
+            else
+                % retain selection
+            end
+            
+            % Call superclass method
+            removeChild@uix.mixin.Container( obj, child )
+            
+        end % removeChild
+        
+        function reorder( obj, indices )
+            %reorder  Reorder contents
+            %
+            %  c.reorder(i) reorders the container contents using indices
+            %  i, c.Contents = c.Contents(i).
+            
+            % Reorder
+            selection = obj.Selection_;
+            if selection ~= 0
+                obj.Selection_ = find( indices == selection );
+            end
+            
+            % Call superclass method
+            reorder@uix.mixin.Container( obj, indices )
+            
+        end % reorder
         
     end % template methods
     
