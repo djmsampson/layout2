914e7141be58ac8f4f4da1b77bc3486bd1beee71
David Sampson <dsampson@mathworks.com>
2014-10-08 16:35:21 +0000

    Added documentation generation to release.m
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@1037 4f12366f-9e95-4241-96af-8f93ba654eeb

--- code/release.m
+++ code/release.m
@@ -1,21 +1,92 @@
-function release
+function release( mode )
 %release  Build, test and package GUI Layout Toolbox
+%
+%  release builds, tests and packages.
+%
+%  release -verbose displays detailed information.
 
 %  Copyright 2009-2014 The MathWorks, Inc.
 %  $Revision: 921 $ $Date: 2014-06-03 11:11:36 +0100 (Tue, 03 Jun 2014) $
 
-% Grab this directory and the current directory
-thisDir = fileparts( mfilename( 'fullpath' ) );
+%% Check inputs
+switch nargin
+    case 0
+        verbose = false;
+    case 1
+        if ischar( mode ) && strcmp( mode, '-verbose' )
+            verbose = true;
+        else
+            error( 'Invalid mode.' )
+        end
+    otherwise
+        narginchk( 0, 1 )
+end
+
+%% Grab this directory and the current directory
+codeDir = fileparts( mfilename( 'fullpath' ) );
 currentDir = pwd;
 
-% Check MATLAB and related tools
+%% Check MATLAB and related tools
 assert( ~verLessThan( 'MATLAB', '8.4' ), 'MATLAB R2014b or higher is required.' )
 assert( ~isempty( ver( 'DocToolsHelp' ) ), 'DocTools is required.' )
 assert( ~isempty( ver( 'xunit' ) ), 'xUnit is required.' )
 
-% Run tests
+%% Check installation
+fprintf( 1, 'Checking installation...' );
+v = ver( 'layout' );
+switch numel( v )
+    case 0
+        fprintf( 1, ' failed.\n' );
+        error( 'GUI Layout Toolbox not found.' )
+    case 1
+        % OK so far
+    otherwise
+        fprintf( 1, ' failed.\n' );
+        error( 'There are multiple copies of GUI Layout Toolbox on the MATLAB path.' )
+end
+if strncmp( which( 'layoutRoot' ), codeDir, numel( codeDir ) )
+    fprintf( 1, ' OK.\n' );
+else
+    fprintf( 1, ' failed.\n' );
+    error( 'GUI Layout Toolbox code at %s is not on the MATLAB path.', codeDir )
+end
+
+%% Build documentation
+fprintf( 1, 'Generating documentation...' );
+try
+    cd( fullfile( fileparts( codeDir ), 'docsrc' ) )
+    log = evalc( 'buildDoc' );
+    close( 'all', 'force' )
+    drawnow()
+    cd( codeDir )
+    fprintf( 1, ' OK.\n' );
+    if verbose
+        fprintf( 1, '%s', log );
+    end
+catch e
+    cd( codeDir )
+    fprintf( 1, ' failed.\n' );
+    e.rethrow()
+end
+
+%% Build examples
+fprintf( 1, 'Generating examples...' );
+try
+    cd( fullfile( fileparts( codeDir ), 'docsrc' ) )
+    buildDocExamples()
+    close all force
+    drawnow
+    cd( codeDir )
+    fprintf( 1, ' OK.\n' );
+catch e
+    cd( codeDir )
+    fprintf( 1, ' OK.\n' );
+    e.rethrow()
+end
+
+%% Run tests
 fprintf( 1, 'Running tests...' );
-cd( fullfile( fileparts( thisDir ), 'tests' ) )
+cd( fullfile( fileparts( codeDir ), 'tests' ) )
 [log, ok] = evalc( 'runtests' );
 cd( currentDir )
 if ok
@@ -25,27 +96,15 @@ else
     error( '%s', log )
 end
 
-% Check installation
-v = ver( 'layout' );
-switch numel( v )
-    case 0
-        error( 'GUI Layout Toolbox not found.' )
-    case 1
-        assert( strncmp( which( 'layoutRoot' ), thisDir, numel( thisDir ) ), ...
-            'GUI Layout Toolbox code at %s is not on the MATLAB path.', thisDir )
-    otherwise
-        error( 'There are multiple copies of GUI Layout Toolbox on the MATLAB path.' )
-end
-
-% Package and rename
+%% Package and rename
 fprintf( 1, 'Packaging...' );
 try
-    prj = fullfile( thisDir, 'GUI Layout Toolbox.prj' );
+    prj = fullfile( codeDir, 'GUI Layout Toolbox.prj' );
     name = char( com.mathworks.toolbox_packaging.services.ToolboxPackagingService.openProject( prj ) );
     com.mathworks.toolbox_packaging.services.ToolboxPackagingService.packageProject( name )
     com.mathworks.toolbox_packaging.services.ToolboxPackagingService.closeProject( name )
-    oldMltbx = fullfile( thisDir, [name '.mltbx'] );
-    newMltbx = fullfile( fileparts( thisDir ), 'releases', [name ' ' v.Version '.mltbx'] );
+    oldMltbx = fullfile( codeDir, [name '.mltbx'] );
+    newMltbx = fullfile( fileparts( codeDir ), 'releases', [name ' ' v.Version '.mltbx'] );
     movefile( oldMltbx, newMltbx )
     fprintf( 1, ' OK.\n' );
 catch e
@@ -53,7 +112,7 @@ catch e
     e.rethrow()
 end
 
-% Check package
+%% Check package
 fprintf( 1, 'Checking package...' );
 info = mlAddonGetProperties( newMltbx );
 if strcmp( info.version, v.Version )
@@ -63,7 +122,7 @@ else
     error( 'Package version %s does not match code version %s.', info.version, v.Version )
 end
 
-% Show message
+%% Show message
 fprintf( 1, 'Created package %s\n', newMltbx );
 
 end % release
