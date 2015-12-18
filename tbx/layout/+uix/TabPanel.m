058b673a155b02a20949d9700a0c7a17a4495ad7
David Sampson <dsampson@mathworks.com>
2015-12-18 23:33:06 +0000

    Backed out changes to uix.TabPanel/removeChild
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@1216 4f12366f-9e95-4241-96af-8f93ba654eeb

--- tbx/layout/+uix/TabPanel.m
+++ tbx/layout/+uix/TabPanel.m
@@ -654,25 +654,8 @@ classdef TabPanel < uix.Container & uix.mixin.Panel
             obj.Tabs(index,:) = [];
             obj.TabListeners(index,:) = [];
             
-            % Adjust selection if required
-            oldSelection = obj.Selection_;
-            if index < oldSelection
-                newSelection = oldSelection - 1;
-            elseif index == oldSelection
-                newSelection = min( oldSelection, numel( contents ) - 1 );
-            else % index > oldSelection
-                newSelection = oldSelection;
-            end
-            obj.Selection_ = newSelection;
-            
             % Call superclass method
-            removeChild@uix.mixin.Container( obj, child )
-            
-            % Notify selection change
-            if oldSelection ~= newSelection
-                obj.notify( 'SelectionChanged', ...
-                    uix.SelectionData( oldSelection, newSelection ) )
-            end
+            removeChild@uix.mixin.Panel( obj, child )
             
         end % removeChild
         
