81a7041523b4c8e0e669dc43e59b6c7b2e057c72
David Sampson <dsampson@mathworks.com>
2016-08-16 00:44:20 +0000

    Updated
    git-svn-id: http://svn.mathworks.co.uk:8080/internalprojects/Z-MLADJS-298/trunk/code/Layouts2@1335 4f12366f-9e95-4241-96af-8f93ba654eeb

--- demos/glt_tmw.m
+++ demos/glt_tmw.m
@@ -5,12 +5,12 @@ f = figure();
 b = uix.HBox( 'Parent', f, 'Padding', 10, 'Spacing', 10 );
 
 % Create some content
-b1 = uicontrol( 'Parent', b, 'String', 'The' );
+c1 = uicontrol( 'Parent', b, 'String', 'The' );
 a = axes( 'Parent', b, 'ActivePositionProperty', 'position', 'Visible', 'off' );
 peaks() % membrane
 a.Visible = 'off';
-b2 = uicontrol( 'Parent', b, 'String', 'Math' );
-b3 = uicontrol( 'Parent', b, 'String', 'Works' );
+c2 = uicontrol( 'Parent', b, 'String', 'Math' );
+c3 = uicontrol( 'Parent', b, 'String', 'Works' );
 
 % Set sizes
 b.Widths = [100 -2 -1 -1];
