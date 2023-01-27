053467d0aa086d621c611d0859ceb05c7694e9c3
Ken Deeley <kdeeley@mathworks.com>
2023-01-27 18:38:58 +0000

    Added string support for uix.BoxPanel properties

--- tests/runToolboxTests.m
+++ tests/runToolboxTests.m
@@ -1,7 +1,16 @@
 function results = runToolboxTests()
 %RUNTOOLBOXTESTS Run the GUI Layout Toolbox tests.
 
+% Identify the current folder.
 rootFolder = fileparts( mfilename( 'fullpath' ) );
+
+% Disable the warning about name conflicts.
+ID = "MATLAB:dispatcher:nameConflict";
+w = warning( "query", ID );
+warningCleanup = onCleanup( @() warning( w ) );
+warning( "off", ID )
+
+% Run the tests.
 results = runtests( rootFolder, 'IncludeSubfolders', true, ...
     'IncludeSubpackages', true );
 
