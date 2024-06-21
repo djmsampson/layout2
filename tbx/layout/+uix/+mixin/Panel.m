278d96b7aa1d4294de4045105bb31e7a70ee8ced
David Sampson <dsampson@mathworks.com>
2024-06-21 11:09:46 +0100

    Whitespace

--- tbx/layout/+uix/+mixin/Panel.m
+++ tbx/layout/+uix/+mixin/Panel.m
@@ -3,35 +3,35 @@ classdef Panel < uix.mixin.Container
     %
     %  uix.mixin.Panel is a mixin class used by panels to provide various
     %  properties and template methods.
-    
+
     %  Copyright 2009-2020 The MathWorks, Inc.
-    
+
     properties( Access = public, Dependent, AbortSet )
         Selection % selected contents
     end
-    
+
     properties( Access = protected )
         Selection_ = 0 % backing for Selection
     end
-    
+
     properties( Access = protected )
         G1218142 = false % bug flag
     end
-    
+
     events( NotifyAccess = protected )
         SelectionChanged % selection changed
     end
-    
+
     methods
-        
+
         function value = get.Selection( obj )
-            
+
             value = obj.Selection_;
-            
+
         end % get.Selection
-        
+
         function set.Selection( obj, value )
-            
+
             % Check
             assert( isa( value, 'double' ), 'uix:InvalidPropertyValue', ...
                 'Property ''Selection'' must be of type double.' )
@@ -49,54 +49,54 @@ classdef Panel < uix.mixin.Container
                 assert( value >= 1 && value <= n, 'uix:InvalidPropertyValue', ...
                     'Property ''Selection'' must be between 1 and the number of children.' )
             end
-            
+
             % Set
             oldSelection = obj.Selection_;
             newSelection = value;
             obj.Selection_ = newSelection;
-            
+
             % Show selected child
             obj.showSelection()
-            
+
             % Mark as dirty
             obj.Dirty = true;
-            
+
             % Raise event
             notify( obj, 'SelectionChanged', ...
                 uix.SelectionData( oldSelection, newSelection ) )
-            
+
         end % set.Selection
-        
+
     end % accessors
-    
+
     methods( Access = protected )
-        
+
         function addChild( obj, child )
-            
+
             % Check for bug
             if verLessThan( 'MATLAB', '8.5' ) && strcmp( child.Visible, 'off' )
                 obj.G1218142 = true;
             end
-            
+
             % Select new content
             oldSelection = obj.Selection_;
             newSelection = numel( obj.Contents_ ) + 1;
             obj.Selection_ = newSelection;
-            
+
             % Call superclass method
             addChild@uix.mixin.Container( obj, child )
-            
+
             % Show selected child
             obj.showSelection()
-            
+
             % Notify selection change
             obj.notify( 'SelectionChanged', ...
                 uix.SelectionData( oldSelection, newSelection ) )
-            
+
         end % addChild
-        
+
         function removeChild( obj, child )
-            
+
             % Adjust selection if required
             contents = obj.Contents_;
             index = find( contents == child );
@@ -109,44 +109,44 @@ classdef Panel < uix.mixin.Container
                 newSelection = oldSelection;
             end
             obj.Selection_ = newSelection;
-            
+
             % Call superclass method
             removeChild@uix.mixin.Container( obj, child )
-            
+
             % Show selected child
             obj.showSelection()
-            
+
             % Notify selection change
             if oldSelection ~= newSelection
                 obj.notify( 'SelectionChanged', ...
                     uix.SelectionData( oldSelection, newSelection ) )
             end
-            
+
         end % removeChild
-        
+
         function reorder( obj, indices )
             %reorder  Reorder contents
             %
             %  c.reorder(i) reorders the container contents using indices
             %  i, c.Contents = c.Contents(i).
-            
+
             % Reorder
             selection = obj.Selection_;
             if selection ~= 0
                 obj.Selection_ = find( indices == selection );
             end
-            
+
             % Call superclass method
             reorder@uix.mixin.Container( obj, indices )
-            
+
         end % reorder
-        
+
         function showSelection( obj )
             %showSelection  Show selected child, hide the others
             %
             %  c.showSelection() shows the selected child of the container
             %  c, and hides the others.
-            
+
             % Set positions and visibility
             selection = obj.Selection_;
             children = obj.Contents_;
@@ -179,9 +179,9 @@ classdef Panel < uix.mixin.Container
                     end
                 end
             end
-            
+
         end % showSelection
-        
+
     end % template methods
-    
+
 end % classdef
