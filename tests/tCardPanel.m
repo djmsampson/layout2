9e848dd4d5d73bc7252a33bc46bba2d27d012a47
David Sampson <dsampson@mathworks.com>
2024-06-29 13:32:00 +0100

    tCardPanel tDeletingChildrenUnsetsSelection

--- tests/tCardPanel.m
+++ tests/tCardPanel.m
@@ -1,6 +1,6 @@
 classdef tCardPanel < sharedtests.SharedPanelTests
     %TCARDPANEL Tests for uiextras.CardPanel and uix.CardPanel.
-    
+
     properties ( TestParameter )
         % The constructor name, or class, of the component under test.
         ConstructorName = {'uiextras.CardPanel', 'uix.CardPanel'}
@@ -8,46 +8,44 @@ classdef tCardPanel < sharedtests.SharedPanelTests
         % constructor and get/set methods.
         NameValuePairs = cardPanelNameValuePairs()
     end % properties ( TestParameter )
-    
+
     methods ( Test, Sealed )
-        
-        function tDeletingChildrenSetsSelectionToZero( testCase )
-            
+
+        function tDeletingChildrenUnsetsSelection( testCase )
+
             % Create a card panel with controls.
             cardPanel = testCase.createCardPanelWithControls();
-            
+
             % Delete all the children.
-            delete( cardPanel.Children )
-            
+            delete( cardPanel.Contents )
+
             % Verify that the 'Selection' property is equal to 0.
             testCase.verifyEqual( cardPanel.Selection, 0, ...
-                ['The ''Selection'' property of the CardPanel ', ...
-                'was not set to 0 when all the children of the ', ...
-                'CardPanel were deleted.'] )
-            
-        end % tDeletingChildrenSetsSelectionToZero
-        
+                'The selection of the CardPanel was not unset when all the children were deleted.' )
+
+        end % tDeletingChildrenUnsetsSelection
+
         function tAddingChildIncrementsSelection( testCase )
-            
+
             % Create a card panel with controls.
             cardPanel = testCase.createCardPanelWithControls();
-            
+
             % Verify that the third control is selected.
             testCase.verifyEqual( cardPanel.Selection, 3, ...
                 ['The CardPanel has not correctly updated its ', ...
                 '''Selection'' property when a new child was added.'] )
-            
+
         end % tAddingChildIncrementsSelection
-        
+
         function tDeletingLowerIndexChildDecrementsSelection( testCase )
-            
+
             % Create a card panel with controls.
             cardPanel = testCase.createCardPanelWithControls();
-            
+
             % Record the current selection, then delete the first child.
             currentSelection = cardPanel.Selection;
-            delete( cardPanel.Children(3) )
-            
+            delete( cardPanel.Contents(3) )
+
             % Test that deleting a child with a lower index than the
             % current selection causes the selection index to decrease by
             % 1.
@@ -56,20 +54,20 @@ classdef tCardPanel < sharedtests.SharedPanelTests
                 ['The CardPanel has not correctly updated its ', ...
                 '''Selection'' property when a child with a lower ', ...
                 'index than the current selection was deleted.'] )
-            
+
         end % tDeletingLowerIndexChildDecrementsSelection
-        
+
         function tDeletingSelectedChildPreservesSelection( testCase )
-            
+
             % Create a card panel with controls.
             cardPanel = testCase.createCardPanelWithControls();
-            
+
             % Select the second child, record the current selection, then
             % delete the second child.
             cardPanel.Selection = 2;
             currentSelection = cardPanel.Selection;
-            delete( cardPanel.Children(2) )
-            
+            delete( cardPanel.Contents(2) )
+
             % Verify that the 'Selection' property has remained the same.
             testCase.verifyEqual( cardPanel.Selection, ...
                 currentSelection, ...
@@ -77,20 +75,20 @@ classdef tCardPanel < sharedtests.SharedPanelTests
                 'has not remained the same when the current child ', ...
                 'was deleted (and the current child was not the ', ...
                 'highest index child).'] )
-            
+
         end % tDeletingSelectedChildPreservesSelection
-        
+
         function tDeletingHigherIndexChildPreservesSelection( testCase )
-            
+
             % Create a card panel with controls.
             cardPanel = testCase.createCardPanelWithControls();
-            
+
             % Select the second child, record the current selection, and
             % delete the third child.
             cardPanel.Selection = 2;
             currentSelection = cardPanel.Selection;
-            delete( cardPanel.Children(1) )
-            
+            delete( cardPanel.Contents(1) )
+
             % Verify that the 'Selection' property has remained the same.
             testCase.verifyEqual( cardPanel.Selection, ...
                 currentSelection, ...
@@ -98,15 +96,15 @@ classdef tCardPanel < sharedtests.SharedPanelTests
                 'has not remained the same when a higher index child ', ...
                 'was deleted (and the current child was not the ', ...
                 'highest index child).'] )
-            
+
         end % tDeletingHigherIndexChildPreservesSelection
-        
+
     end % methods ( Test, Sealed )
-    
+
     methods ( Access = private )
-        
+
         function cardPanel = createCardPanelWithControls( testCase )
-            
+
             % Create a CardPanel with three controls.
             parent = testCase.ParentFixture.Parent;
             cardPanelConstructor = testCase.ConstructorName{1};
@@ -121,11 +119,11 @@ classdef tCardPanel < sharedtests.SharedPanelTests
             uicontrol( 'Parent', cardPanel, ...
                 'Style', 'frame', ...
                 'BackgroundColor', 'b' )
-            
+
         end % createCardPanelWithControls
-        
+
     end % methods ( Access = private )
-    
+
 end % classdef
 
 function nvp = cardPanelNameValuePairs()
