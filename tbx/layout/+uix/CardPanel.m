d4c45d942e030a8c3741caf4e99f0226e1a44957
David Sampson <dsampson@mathworks.com>
2024-06-30 21:07:35 +0100

    CardPanel set child visiblility based on selection, not literal

--- tbx/layout/+uix/CardPanel.m
+++ tbx/layout/+uix/CardPanel.m
@@ -126,7 +126,8 @@ classdef CardPanel < uix.Container & uix.mixin.Container
             if oldSelection ~= 0
                 uix.mixin.Container.setVisible( oldContents(oldSelection), 'off' )
             end
-            uix.mixin.Container.setVisible( child, 'on', 0.04 ) % async
+            uix.mixin.Container.setVisible( child, ...
+                @()isequal( obj.Contents(obj.Selection), child ), 0.04 ) % async
 
             % Mark as dirty
             obj.Dirty = true;
