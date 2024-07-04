13c759b0c14e1a828e11fd6e50ca6436f1c828d0
David Sampson <dsampson@mathworks.com>
2024-07-04 14:17:18 +0100

    Removed uix.warning

--- tests/+glttestutilities/TestInfrastructure.m
+++ tests/+glttestutilities/TestInfrastructure.m
@@ -111,7 +111,7 @@ classdef ( Abstract ) TestInfrastructure < matlab.unittest.TestCase
             warningID = 'uix:Deprecated';
             warningState = warning( 'query', warningID );
             testCase.addTeardown( @() warning( warningState ) )
-            uix.warning( 'off', warningID )
+            warning( 'off', warningID )
 
         end % disableDeprecationWarning
 
