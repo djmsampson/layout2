65d7be9c7681bcad63dc87e17ca6bd392861c582
David Sampson <dsampson@mathworks.com>
2024-09-02 17:26:12 +0100

    Error messages in uix.ButtonBox

--- tbx/layout/+uix/ButtonBox.m
+++ tbx/layout/+uix/ButtonBox.m
@@ -2,98 +2,106 @@ classdef ButtonBox < uix.Box
     %uix.ButtonBox  Button box base class
     %
     %  uix.ButtonBox is a base class for containers that lay out buttons.
-    
+
     %  Copyright 2009-2020 The MathWorks, Inc.
-    
+
     properties( Access = public, Dependent, AbortSet )
         ButtonSize % button size, in pixels
         HorizontalAlignment % horizontal alignment [left|center|right]
         VerticalAlignment % vertical alignment [top|middle|bottom]
     end
-    
+
     properties( Access = protected )
         ButtonSize_ = [60 20] % backing for ButtonSize
         HorizontalAlignment_ = 'center' % backing for HorizontalAlignment
         VerticalAlignment_ = 'middle' % backing for VerticalAlignment
     end
-    
+
     methods
-        
+
         function value = get.ButtonSize( obj )
-            
+
             value = obj.ButtonSize_;
-            
+
         end % get.ButtonSize
-        
+
         function set.ButtonSize( obj, value )
-            
+
             % Check
-            assert( isa( value, 'double' ), 'uix:InvalidPropertyValue', ...
-                'Property ''ButtonSize'' must be of type double.' )
-            assert( isequal( size( value ), [1 2] ), ...
-                'uix:InvalidPropertyValue', ...
-                'Size of property ''ButtonSize'' must by 1-by-2.' )
-            assert( all( isreal( value ) ) && ~any( isinf( value ) ) && ...
-                ~any( isnan( value ) ) && ~any( value <= 0 ), ...
-                'uix:InvalidPropertyValue', ...
-                'Elements of property ''ButtonSize'' must be real, finite and positive.' )
-            
+            try
+                value = double( value ); % convert
+                assert( isequal( size( value ), [1 2] ) && ...
+                    all( isreal( value ) ) && ~any( isinf( value ) ) && ...
+                    ~any( isnan( value ) ) && all( value > 0 ) )
+            catch
+                throwAsCaller( MException( 'uix:InvalidPropertyValue', ...
+                    'Error setting property ''%s'' of class ''%s''.\n%s', ...
+                    'ButtonSize', class( obj ), ...
+                    'Value must be a 1x2 positive double array.' ) )
+            end
+
             % Set
             obj.ButtonSize_ = value;
-            
+
             % Mark as dirty
             obj.Dirty = true;
-            
+
         end % set.ButtonSize
-        
+
         function value = get.HorizontalAlignment( obj )
-            
+
             value = obj.HorizontalAlignment_;
-            
+
         end % get.HorizontalAlignment
-        
+
         function set.HorizontalAlignment( obj, value )
-            
+
             % Check
             try
+                value = char( value ); % convert
                 assert( ismember( value, {'left';'center';'right'} ) )
             catch
-                error( 'uix:InvalidPropertyValue', ...
-                    'Property ''HorizontalAlignment'' must be ''left'', ''center'' or ''right''.' )
-            end % try/catch
-            
+                throwAsCaller( MException( 'uix:InvalidPropertyValue', ...
+                    'Error setting property ''%s'' of class ''%s''.\n%s', ...
+                    'HorizontalAlignment', class( obj ), ...
+                    'Value must be ''left'', ''right'', or ''center''.' ) )
+            end
+
             % Set
-            obj.HorizontalAlignment_ = char( value );
-            
+            obj.HorizontalAlignment_ = value;
+
             % Mark as dirty
             obj.Dirty = true;
-            
+
         end % set.HorizontalAlignment
-        
+
         function value = get.VerticalAlignment( obj )
-            
+
             value = obj.VerticalAlignment_;
-            
+
         end % get.VerticalAlignment
-        
+
         function set.VerticalAlignment( obj, value )
-            
+
             % Check
             try
+                value = char( value ); % convert
                 assert( ismember( value, {'top';'middle';'bottom'} ) )
             catch
-                error( 'uix:InvalidPropertyValue', ...
-                    'Property ''VerticalAlignment'' must be ''top'', ''middle'' or ''bottom''.' )
-            end % try/catch
-            
+                throwAsCaller( MException( 'uix:InvalidPropertyValue', ...
+                    'Error setting property ''%s'' of class ''%s''.\n%s', ...
+                    'VerticalAlignment', class( obj ), ...
+                    'Value must be ''top'', ''bottom'', or ''middle''.' ) )
+            end
+
             % Set
-            obj.VerticalAlignment_ = char( value );
-            
+            obj.VerticalAlignment_ = value;
+
             % Mark as dirty
             obj.Dirty = true;
-            
+
         end % set.VerticalAlignment
-        
+
     end % accessors
-    
+
 end % classdef
