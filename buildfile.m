5179b9cf01b6c098ff345daa61f5ab0fcaa8d500
Ken Deeley <kdeeley@mathworks.com>
2024-12-05 18:44:14 +0000

    Added layout.json and updated buildfile; fixes #2375294

--- buildfile.m
+++ buildfile.m
@@ -1,26 +1,17 @@
 function plan = buildfile()
 %BUILDFILE Build file for managing GUI Layout Toolbox packaging tasks.
 
-% Initialize the build plan from the local task functions below.
-plan = buildplan( localfunctions() );
-projectRoot = plan.RootFolder;
+% Copyright 2024 The MathWorks, Inc.
 
-% Set the project root as the folder in which to check for any static code
-% issues.
-sourceFiles = projectRoot;
-plan("assertNoCodeIssues") = matlab.buildtool.tasks.CodeIssuesTask( ...
-    sourceFiles, ...
-    "IncludeSubfolders", true, ...
-    "Description", ...
-    "Assert that there are no code issues in the project.", ...
-    "WarningThreshold", 0 );
+% Define the build plan.
+plan = buildplan( localfunctions() );
 
 % Add a test task to run the unit tests for the project. Generate and save
 % a coverage report. This build task is optional.
+projectRoot = plan.RootFolder;
 testFolder = fullfile( projectRoot, "tests" );
 codeFolder = fullfile( projectRoot, "tbx", "layout"  );
-plan("assertTestSuccess") = matlab.buildtool.tasks.TestTask( ...
-    testFolder, ...
+plan("test") = matlab.buildtool.tasks.TestTask( testFolder, ...
     "Strict", true, ...
     "Description", "Assert that all tests across the project pass.", ...
     "SourceFiles", codeFolder, ...
@@ -28,143 +19,118 @@ plan("assertTestSuccess") = matlab.buildtool.tasks.TestTask( ...
     "OutputDetail", "none" );
 
 % Set the package toolbox task to run by default.
-plan.DefaultTasks = "packageToolbox";
+plan.DefaultTasks = "package";
 
-% Define the task dependencies.
-coreDependencies = ["assertNoCodeIssues", "assertNoProjectIssues"];
-plan("checkDocerInstallation").Dependencies = coreDependencies;
-plan("deletePreviousDocFiles").Dependencies = "checkDocerInstallation";
-plan("convertMarkdownToHTML").Dependencies = "deletePreviousDocFiles";
-plan("runDocExamples").Dependencies = "convertMarkdownToHTML";
-plan("createDocIndex").Dependencies = "runDocExamples";
-plan("packageToolbox").Dependencies = "createDocIndex";
+% Define the task dependencies and inputs.
+plan("doc").Dependencies = "check";
+plan("doc").Inputs = fullfile( projectRoot, "tbx", "layoutdoc" );
+plan("package").Dependencies = "doc";
 
 end % buildfile
 
-function assertNoProjectIssuesTask( ~ )
-% Assert that there are no project issues.
+function checkTask( context )
+% Check the source code and project for any issues.
 
-proj = currentProject();
-proj.updateDependencies()
-results = table( proj.runChecks() );
-disp( results(~results.Passed, :) )
-assert( all( results.Passed ), "buildfile:ProjectIssue", ...
+% Set the project root as the folder in which to check for any static code
+% issues.
+projectRoot = context.Plan.RootFolder;
+codeIssuesTask = matlab.buildtool.tasks.CodeIssuesTask( projectRoot, ...
+     "IncludeSubfolders", true, ...
+     "Configuration", "factory", ...
+     "Description", ...
+     "Assert that there are no code issues in the project.", ...
+     "WarningThreshold", 0 );
+codeIssuesTask.analyze( context )
+
+% Update the project dependencies.
+prj = currentProject();
+prj.updateDependencies()
+
+% Run the checks.
+checkResults = table( prj.runChecks() );
+
+% Log any failed checks.
+passed = checkResults.Passed;
+notPassed = ~passed;
+if any( notPassed )
+    disp( checkResults(notPassed, :) )
+else
+    fprintf( "** All project checks passed.\n\n" )    
+end % if
+
+% Check that all checks have passed.
+assert( all( passed ), "buildfile:ProjectIssue", ...
     "At least one project check has failed. " + ...
     "Resolve the failures shown above to continue." )
 
-end % assertNoProjectIssuesTask
+end % checkTask
 
-function checkDocerInstallationTask( ~ )
-% Check that the Doc_er toolbox is installed.
+function docTask( context )
+% Generate the GUI Layout Toolbox documentation.
 
+% Check that Doc_er is installed.
 v = ver( "docer" );
 assert( ~isempty( v ), "buildfile:NoDocer", ...
     "Building GUI Layout Toolbox documentation requires Doc_er." )
+fprintf( "** Using " + v.Name + " version " + v.Version + " " + ...
+    v.Release + ", " + v.Date + ", for this build.\n\n" )
 
-end % checkDocerInstallationTask
-
-function deletePreviousDocFilesTask( context )
-% Use Doc_er to delete any previous documentation files.
-
-docFolder = layoutDocRoot( context );
+% Delete the old documentation files.
+docFolder = context.Task.Inputs.Path;
 docerdelete( docFolder )
+fprintf( "** Deleted the old documentation files.\n\n" )
 
-end % deletePreviousDocFilesTask
-
-function convertMarkdownToHTMLTask( context )
 % Use Doc_er to convert the markdown (.md) files to HTML.
-
-docFolder = layoutDocRoot( context );
 allMDFiles = fullfile( docFolder, "**", "*.md" );
 docerconvert( allMDFiles )
+fprintf( "** Converted markdown files to HTML.\n\n" )
 
-end % convertMarkdownToHTMLTask
-
-function runDocExamplesTask( context )
 % Use Doc_er to evaluate and capture the code within the doc pages.
-
-docFolder = layoutDocRoot( context );
 allHTMLFiles = fullfile( docFolder, "**", "*.html" );
 docerrun( allHTMLFiles )
+fprintf( "** Evaluated and captured documentation example code.\n\n" )
 
-end % runDocExamplesTask
-
-function createDocIndexTask( context )
 % Use Doc_er to create the documentation index files and search database.
-
-docFolder = layoutDocRoot( context );
 docerindex( docFolder )
+fprintf( "** Created the index and search database files.\n\n" )
 
-end % createDocIndexTask
+end % docTask
 
-function packageToolboxTask( context )
+function packageTask( context )
 % Package the GUI Layout Toolbox.
 
-% Write down the toolbox root directory and ID.
+% Toolbox short name.
+toolboxShortName = "layout";
+
+% Project root directory.
 projectRoot = context.Plan.RootFolder;
-toolboxRoot = fullfile( projectRoot, "tbx" );
-toolboxID = "e5af5a78-4a80-11e4-9553-005056977bd0";
 
-% Create the toolbox packaging options.
-opts = matlab.addons.toolbox.ToolboxOptions( toolboxRoot, toolboxID );
+% Import and tweak the toolbox metadata.
+toolboxJSON = fullfile( projectRoot, toolboxShortName + ".json" );
+meta = jsondecode( fileread( toolboxJSON ) );
+meta.ToolboxMatlabPath = fullfile( projectRoot, meta.ToolboxMatlabPath );
+meta.ToolboxFolder = fullfile( projectRoot, meta.ToolboxFolder );
+meta.ToolboxImageFile = fullfile( projectRoot, meta.ToolboxImageFile );
+versionString = feval( @(s) s(1).Version, ver( toolboxShortName ) ); %#ok<FVAL>
+meta.ToolboxVersion = versionString;
+toolboxMLTBX = meta.ToolboxName + " " + versionString + ".mltbx";
+% toolboxMLTBX = fullfile( projectRoot, "releases", ...
+%     meta.ToolboxName + " " + versionString + ".mltbx" );
+meta.OutputFile = toolboxMLTBX; 
 
-% Fill in the options.
-opts.AuthorCompany = "MathWorks";
-opts.AuthorEmail = "dsampson@mathworks.com";
-opts.AuthorName = "David Sampson";
-opts.Description = "This toolbox provides tools to create " + ...
-    "sophisticated MATLAB graphical user interfaces that resize " + ...
-    "gracefully.  The classes supplied can be used in combination" + ...
-    " to produce virtually any user interface layout." + newline() + ...
-    newline() + ...
-    "* Arrange MATLAB user interface components horizontally, " + ...
-    "vertically or in grids" + newline() + ...
-    "* Mix fixed- and variable-size components" + newline() + ...
-    "* Resize components interactively by dragging dividers" + ...
-    newline() + ...
-    "* Show and hide components using tabs and panels" + newline() + ...
-    "* Show part of a large component in a scrollable panel" + ...
-    newline() + newline() + ...
-    "This toolbox was developed by David Sampson and Ben Tordoff " + ...
-    "from the [Consulting Services]" + ...
-    "(http://www.mathworks.com/services/consulting/) group at " + ...
-    "MathWorks." + newline() + newline() + ...
-    "This version is for MATLAB R2014b and later. For R2014a and " + ...
-    "earlier, see [version 1](http://www.mathworks.com/" + ...
-    "matlabcentral/fileexchange/27758-gui-layout-toolbox).";
+% Define the toolbox packaging options.
+toolboxFolder = meta.ToolboxFolder;
+toolboxID = meta.Identifier;
+meta = rmfield( meta, ["Identifier", "ToolboxFolder"] );
+opts = matlab.addons.toolbox.ToolboxOptions( ...
+    toolboxFolder, toolboxID, meta );
 
-% Read the version number from Contents.m.
-contentsFile = fullfile( toolboxRoot, "layout", "Contents.m" );
-contentsFileText = fileread( contentsFile );
-versionNumber = string( extractBetween( ...
-    contentsFileText, "Version ", " (" ) );
-mltbxName = "GUI Layout Toolbox " + versionNumber + ".mltbx";
-
-% Place the .mltbx file in the project root.
-opts.OutputFile = fullfile( projectRoot, mltbxName );
-
-% Continue filling in the options.
-opts.Summary = "Layout manager for MATLAB graphical user interfaces";
-opts.SupportedPlatforms = struct( "Win64", true, ...
-    "Glnxa64", true, ...
-    "Maci64", true, ...
-    "MatlabOnline", true );
-opts.ToolboxGettingStartedGuide = "";
-opts.ToolboxImageFile = fullfile( projectRoot, "glt.png" );
-opts.ToolboxName = "GUI Layout Toolbox";
-opts.ToolboxVersion = versionNumber;
-opts.MinimumMatlabRelease = "R2014b";
-opts.MaximumMatlabRelease = ""; % Current version
+% Remove the markdown (.md) files.
+markdownFilesIdx = endsWith( opts.ToolboxFiles, ".md" );
+opts.ToolboxFiles(markdownFilesIdx) = [];
 
 % Package the toolbox.
 matlab.addons.toolbox.packageToolbox( opts )
+fprintf( 1, "[+] %s\n", opts.OutputFile )
 
-end % packageToolboxTask
-
-function docFolder = layoutDocRoot( context )
-%LAYOUTDOCROOT Return the layout doc root from the build context.
-
-projectRoot = context.Plan.RootFolder;
-docFolder = fullfile( projectRoot, "tbx", "layoutdoc" );
-
-end % layoutDocRoot
+end % packageTask
